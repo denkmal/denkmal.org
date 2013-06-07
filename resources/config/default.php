5356453c878aa285e499541d05c28560b089ebf1
Reto Kaiser <reto@cargomedia.ch>
2013-06-07 19:04:17 +0200

    Set db config

--- resources/config/default.php
+++ resources/config/default.php
@@ -2,7 +2,7 @@
 
 $config->debug = true;
 
-$config->CM_Db_Db->db = '';
-$config->CM_Db_Db->username = '';
-$config->CM_Db_Db->password = '';
+$config->CM_Db_Db->db = 'denkmal';
+$config->CM_Db_Db->username = 'root';
+$config->CM_Db_Db->password = 'root';
 $config->CM_Db_Db->server = array('host' => '127.0.0.1', 'port' => 3306);

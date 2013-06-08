45aa1829dadc1c6e309f056ea8bb559f6c99c851
Tomasz Durka <tomasz@durka.pl>
2013-06-08 16:30:44 +0200

    Add EventAdd component skeleton

---
+++ library/Denkmal/library/Denkmal/Component/EventAdd.js
@@ -0,0 +1,9 @@
+/**
+ * @class Denkmal_Component_EventAdd
+ * @extends Denkmal_Component_Abstract
+ */
+var Denkmal_Component_EventAdd = Denkmal_Component_Abstract.extend({
+
+	/** @type String */
+	_class: 'Denkmal_Component_EventAdd'
+});

9c6792ce0254c14188fd635d4653f6ea48835ecb
Tomasz Durka <tomasz@durka.pl>
2013-06-08 12:17:55 +0200

    Add song model

---
+++ library/Denkmal/library/Denkmal/Model/Song.php
@@ -0,0 +1,11 @@
+<?php
+
+class Denkmal_Model_Song extends CM_Model_Abstract {
+
+	const TYPE = 102;
+
+	protected function _loadData () {
+		CM_Db_Db::select('denkmal_song', array('*'), array('id' => $this->getId()))->fetch();
+	}
+
+}

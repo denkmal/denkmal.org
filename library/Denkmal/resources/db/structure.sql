c89891895e7ec8693f8f09c1b97917d5c58f0858
Tomasz Durka <tomasz@durka.pl>
2013-06-08 11:52:10 +0200

    Distribute status into queued, enabled, hidden columns

--- library/Denkmal/resources/db/structure.sql
+++ library/Denkmal/resources/db/structure.sql
@@ -1,11 +1,31 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
-
 DROP TABLE IF EXISTS `denkmal_event`;
-DROP TABLE IF EXISTS `denkmal_venueAlias`;
-DROP TABLE IF EXISTS `denkmal_venue`;
-DROP TABLE IF EXISTS `denkmal_song`;
 
+
+CREATE TABLE `denkmal_event` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `venueId` int(11) unsigned NOT NULL,
+  `from` int(11) unsigned NOT NULL,
+  `until` int(11) unsigned DEFAULT NULL,
+  `description` text,
+  `songId` int(11) unsigned DEFAULT NULL,
+  `queued` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  `enabled` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  `star` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `venueId` (`venueId`),
+  KEY `from` (`from`),
+  KEY `hidden` (`hidden`),
+  KEY `enabled` (`enabled`),
+  KEY `queued` (`queued`),
+  KEY `songId` (`songId`),
+  CONSTRAINT `denkmal_event__venue` FOREIGN KEY (`venueId`) REFERENCES `denkmal_venue` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `denkmal_event__song` FOREIGN KEY (`songId`) REFERENCES `denkmal_song` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `denkmal_song`;
 
 
 CREATE TABLE `denkmal_song` (
@@ -13,6 +33,8 @@ CREATE TABLE `denkmal_song` (
   `label` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `denkmal_venue`;
 
 
 CREATE TABLE `denkmal_venue` (
@@ -22,12 +44,18 @@ CREATE TABLE `denkmal_venue` (
   `address` varchar(200) DEFAULT NULL,
   `latitude` float DEFAULT NULL,
   `longitude` float DEFAULT NULL,
-  `status` tinyint(4) unsigned NOT NULL,
+  `queued` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  `enabled` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) unsigned NOT NULL DEFAULT '0',
   `source` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
-  KEY `status` (`status`)
+  KEY `hidden` (`hidden`),
+  KEY `enabled` (`enabled`),
+  KEY `queued` (`queued`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `denkmal_venueAlias`;
 
 
 CREATE TABLE `denkmal_venueAlias` (
@@ -38,21 +66,3 @@ CREATE TABLE `denkmal_venueAlias` (
   CONSTRAINT `denkmal_venueAlias__venue` FOREIGN KEY (`venueId`) REFERENCES `denkmal_venue` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `denkmal_event` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `venueId` int(11) unsigned NOT NULL,
-  `from` int(11) unsigned NOT NULL,
-  `until` int(11) unsigned DEFAULT NULL,
-  `description` text,
-  `songId` int(11) unsigned DEFAULT NULL,
-  `status` tinyint(4) unsigned NOT NULL,
-  `star` tinyint(4) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `venueId` (`venueId`),
-  KEY `from` (`from`),
-  KEY `status` (`status`),
-  KEY `songId` (`songId`),
-  CONSTRAINT `denkmal_event__venue` FOREIGN KEY (`venueId`) REFERENCES `denkmal_venue` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `denkmal_event__song` FOREIGN KEY (`songId`) REFERENCES `denkmal_song` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;

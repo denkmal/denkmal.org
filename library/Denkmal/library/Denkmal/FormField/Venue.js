a9613af680cef9e8c8d0dcd2476cca0930637a23
Tomasz Durka <tomasz@durka.pl>
2013-06-08 16:19:02 +0200

    Add FormField_Venue

---
+++ library/Denkmal/library/Denkmal/FormField/Venue.js
@@ -0,0 +1,7 @@
+/**
+ * @class Denkmal_FormField_Venue
+ * @extends CM_FormField_SuggestOne
+ */
+var Denkmal_FormField_Venue = CM_FormField_SuggestOne.extend({
+	_class: 'Denkmal_FormField_Venue'
+});

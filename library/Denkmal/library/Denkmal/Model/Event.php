558172bb90cee29a56b9ddd1387ce8f386eefc6d
Reto Kaiser <reto@cargomedia.ch>
2013-06-08 12:58:51 +0200

    Formatting

--- library/Denkmal/library/Denkmal/Model/Event.php
+++ library/Denkmal/library/Denkmal/Model/Event.php
@@ -4,8 +4,7 @@ class Denkmal_Model_Event extends CM_Model_Abstract {
 
 	const TYPE = 101;
 
-	protected function _loadData () {
-		 CM_Db_Db::select('denkmal_event', array('*'), array('id' => $this->getId()))->fetch();
+	protected function _loadData() {
+		CM_Db_Db::select('denkmal_event', array('*'), array('id' => $this->getId()))->fetch();
 	}
-
 }
--- library/Denkmal/library/Denkmal/Model/Venue.php
+++ library/Denkmal/library/Denkmal/Model/Venue.php
@@ -4,8 +4,7 @@ class Denkmal_Model_Venue extends CM_Model_Abstract {
 
 	const TYPE = 100;
 
-	protected function _loadData () {
+	protected function _loadData() {
 		CM_Db_Db::select('denkmal_venue', array('*'), array('id' => $this->getId()))->fetch();
 	}
-
 }

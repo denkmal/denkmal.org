d617715d2e3209a29571b0336741df7d2888b65b
Tomasz Durka <tomasz@durka.pl>
2013-06-08 19:32:19 +0200

    Refactor EventAdd form, adjust fields

--- library/Denkmal/layout/default/Component/EventAdd/default.tpl
+++ library/Denkmal/layout/default/Component/EventAdd/default.tpl
@@ -1,1 +1,18 @@
-Event add
+{form name='Denkmal_Form_EventAdd'}
+	{formField name='venue' label={translate 'Venue'}}
+	<div class="venueDetails">
+		{formField name='venueAddress' label={translate 'Address'}}
+		{formField name='venueUrl' label={translate 'Website'}}
+	</div>
+
+	Event details
+	{formField name='date' label={translate 'From'}}
+	{formField name='fromTime' label={translate 'Time'}}
+	{formField name='untilTime' label={translate 'Until'}}
+	{formField name='title' label={translate 'Title'}}
+	{formField name='artist' label={translate 'Artist'}}
+	{formField name='genres' label={translate 'Genres'}}
+	{formField name='url' label={translate 'Website'}}
+
+	{formAction action='create' label={translate 'Add'}}
+{/form}
--- library/Denkmal/library/Denkmal/Form/EventAdd.php
+++ library/Denkmal/library/Denkmal/Form/EventAdd.php
@@ -4,11 +4,19 @@ class Denkmal_Form_EventAdd extends CM_Form_Abstract {
 
 	public function setup () {
 		$this->registerField(new Denkmal_FormField_Venue('venue'));
-		$this->registerField(new CM_FormField_Integer('int', -10, 20, 2));
-		$this->registerField(new CM_FormField_Distance('locationSlider'));
-		$this->registerField(new CM_FormField_Location('location', null, null, $this->getField('locationSlider')));
-		$this->registerField(new CM_FormField_File('file', 2));
+		$this->registerField(new CM_FormField_Text('venueAddress'));
+		$this->registerField(new CM_FormField_Text('venueUrl'));
 
+		$this->registerField(new CM_FormField_Date('date'));
+		$this->registerField(new CM_FormField_Text('fromTime'));
+		$this->registerField(new CM_FormField_Text('untilTime'));
+
+		$this->registerField(new CM_FormField_Text('title'));
+		$this->registerField(new CM_FormField_Text('artist'));
+		$this->registerField(new CM_FormField_Text('genres'));
+		$this->registerField(new CM_FormField_Text('url'));
+
+		$this->registerAction(new Denkmal_FormAction_EventAdd_Create());
 	}
 
 }

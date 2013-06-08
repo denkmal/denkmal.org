378a8cf764ee591239f8c564d8d95ef2720ca791
Tomasz Durka <tomasz@durka.pl>
2013-06-08 14:36:27 +0200

    Implement message

---
+++ library/Denkmal/library/Denkmal/Model/Message.php
@@ -0,0 +1,46 @@
+<?php
+
+class Denkmal_Model_Message extends CM_Model_Abstract {
+
+	const TYPE = 104;
+
+	/**
+	 * @return string
+	 */
+	public function getText() {
+		return (string) $this->_get('text');
+	}
+
+	/**
+	 * @return Denkmal_Model_Venue
+	 */
+	public function getVenue() {
+		return new Denkmal_Model_Venue($this->_get('venueId'));
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getCreated() {
+		return (int) $this->_get('created');
+	}
+
+	protected function _loadData () {
+		return CM_Db_Db::select('denkmal_message', array('*'), array('id' => $this->getId()))->fetch();
+	}
+
+	protected static function _create(array $data) {
+		$data = Denkmal_Params::factory($data);
+
+		$venue = $data->getVenue('venue');
+		$text = $data->getString('text');
+		$created = time();
+
+		$id = CM_Db_Db::insert('denkmal_message', array('venueId' => $venue->getId(), 'text' => $text, 'created' => $created));
+		return new self($id);
+	}
+
+	protected function _onDelete() {
+		CM_Db_Db::delete('denkmal_message', array('id' => $this->getId()));
+	}
+}
---
+++ tests/library/Denkmal/Model/MessageTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class Denkmal_Model_MessageTest extends CMTest_TestCase {
+
+	protected function tearDown() {
+		CMTest_TH::clearEnv();
+	}
+
+	public function testCreate() {
+		$text = 'foo bar';
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => false));
+		$message = Denkmal_Model_Message::create(array(
+			'text' => $text,
+			'venue' => $venue
+		));
+		$this->assertEquals($venue, $message->getVenue());
+		$this->assertSame($text, $message->getText());
+		$this->assertSameTime(time(), $message->getCreated());
+	}
+
+	/**
+	 * @expectedException CM_Exception_Nonexistent
+	 */
+	public function testOnDelete() {
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => false));
+		$message = Denkmal_Model_Message::create(array(
+			'text' => 'foo',
+			'venue' => $venue
+		));
+		$message->delete();
+		new Denkmal_Model_Message($message->getId());
+	}
+}

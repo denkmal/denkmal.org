d0c5e68ef0b4257b0d2e91f629b0f788fa6298a3
Reto Kaiser <reto@cargomedia.ch>
2013-06-08 13:56:29 +0200

    Add venue getters/setters

--- library/Denkmal/library/Denkmal/Model/Venue.php
+++ library/Denkmal/library/Denkmal/Model/Venue.php
@@ -4,6 +4,170 @@ class Denkmal_Model_Venue extends CM_Model_Abstract {
 
 	const TYPE = 100;
 
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return (string) $this->_get('name');
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName($name) {
+		$name = (string) $name;
+		CM_Db_Db::update('denkmal_venue', array('name' => $name), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getUrl() {
+		$url = $this->_get('url');
+		if (null === $url) {
+			return null;
+		}
+		return (string) $url;
+	}
+
+	/**
+	 * @param string|null $url
+	 */
+	public function setUrl($url) {
+		$url = isset($url) ? (string) $url : null;
+		CM_Db_Db::update('denkmal_venue', array('url' => $url), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getAddress() {
+		$address = $this->_get('address');
+		if (null === $address) {
+			return null;
+		}
+		return (string) $address;
+	}
+
+	/**
+	 * @param string|null $address
+	 */
+	public function setAddress($address) {
+		$address = isset($address) ? (string) $address : null;
+		CM_Db_Db::update('denkmal_venue', array('address' => $address), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getLatitude() {
+		$latitude = $this->_get('latitude');
+		if (null === $latitude) {
+			return null;
+		}
+		return (float) $latitude;
+	}
+
+	/**
+	 * @param float|null $latitude
+	 */
+	public function setLatitude($latitude) {
+		$latitude = isset($latitude) ? (float) $latitude : null;
+		CM_Db_Db::update('denkmal_venue', array('latitude' => $latitude), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getLongitude() {
+		$longitude = $this->_get('longitude');
+		if (null === $longitude) {
+			return null;
+		}
+		return (float) $longitude;
+	}
+
+	/**
+	 * @param float|null $longitude
+	 */
+	public function setLongitude($longitude) {
+		$longitude = isset($longitude) ? (float) $longitude : null;
+		CM_Db_Db::update('denkmal_venue', array('longitude' => $longitude), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getQueued() {
+		return (boolean) $this->_get('queued');
+	}
+
+	/**
+	 * @param boolean $queued
+	 */
+	public function setQueued($queued) {
+		$queued = (boolean) $queued;
+		CM_Db_Db::update('denkmal_venue', array('queued' => $queued), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getEnabled() {
+		return (boolean) $this->_get('enabled');
+	}
+
+	/**
+	 * @param boolean $enabled
+	 */
+	public function setEnabled($enabled) {
+		$enabled = (boolean) $enabled;
+		CM_Db_Db::update('denkmal_venue', array('enabled' => $enabled), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getHidden() {
+		return (boolean) $this->_get('hidden');
+	}
+
+	/**
+	 * @param boolean $hidden
+	 */
+	public function setHidden($hidden) {
+		$hidden = (boolean) $hidden;
+		CM_Db_Db::update('denkmal_venue', array('hidden' => $hidden), array('id' => $this->getId()));
+		$this->_change();
+	}
+
+	/**
+	 * @return int|null
+	 */
+	public function getSource() {
+		$source = $this->_get('source');
+		if (null === $source) {
+			return null;
+		}
+		return (int) $source;
+	}
+
+	/**
+	 * @param int|null $source
+	 */
+	public function setSource($source) {
+		$source = isset($source) ? (int) $source : null;
+		CM_Db_Db::update('denkmal_venue', array('source' => $source), array('id' => $this->getId()));
+		$this->_change();
+	}
+
 	protected function _loadData() {
 		return CM_Db_Db::select('denkmal_venue', array('*'), array('id' => $this->getId()))->fetch();
 	}
--- tests/library/Denkmal/Model/VenueTest.php
+++ tests/library/Denkmal/Model/VenueTest.php
@@ -2,13 +2,102 @@
 
 class Denkmal_Model_VenueTest extends CMTest_TestCase {
 
-	public function testCreate() {
-		$venue = Denkmal_Model_Venue::create(array(
-			'name' => 'Example',
-			'queued' => true,
-			'enabled' => false,
-		));
+	protected function tearDown() {
+		CMTest_TH::clearEnv();
+	}
 
-		$this->assertInstanceOf('Denkmal_Model_Venue', $venue);
+	public function testCreate() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => false));
+
+		$this->assertSame('Example', $venue->getName());
+		$this->assertSame(null, $venue->getAddress());
+		$this->assertSame(null, $venue->getLatitude());
+		$this->assertSame(null, $venue->getLongitude());
+		$this->assertSame(true, $venue->getQueued());
+		$this->assertSame(false, $venue->getEnabled());
+		$this->assertSame(false, $venue->getHidden());
+		$this->assertSame(null, $venue->getSource());
+	}
+
+	public function testGetSetName() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Foo', 'queued' => true, 'enabled' => false));
+		$this->assertSame('Foo', $venue->getName());
+
+		$venue->setName('Bar');
+		$this->assertSame('Bar', $venue->getName());
+	}
+
+	public function testGetSetAddress() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => false, 'address' => 'Foo'));
+		$this->assertSame('Foo', $venue->getAddress());
+
+		$venue->setAddress('Bar');
+		$this->assertSame('Bar', $venue->getAddress());
+	}
+
+	public function testGetSetLatitude() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => false, 'latitude' => '23.5'));
+		$this->assertSame(23.5, $venue->getLatitude());
+
+		$venue->setLatitude(null);
+		$this->assertSame(null, $venue->getLatitude());
+
+		$venue->setLatitude(14.2);
+		$this->assertSame(14.2, $venue->getLatitude());
+	}
+
+	public function testGetSetLongitude() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => false, 'longitude' => '23.5'));
+		$this->assertSame(23.5, $venue->getLongitude());
+
+		$venue->setLongitude(null);
+		$this->assertSame(null, $venue->getLongitude());
+
+		$venue->setLongitude(14.2);
+		$this->assertSame(14.2, $venue->getLongitude());
+	}
+
+	public function testGetSetQueued() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => false));
+		$this->assertSame(true, $venue->getQueued());
+
+		$venue->setQueued(false);
+		$this->assertSame(false, $venue->getQueued());
+	}
+
+	public function testGetSetEnabled() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => true));
+		$this->assertSame(true, $venue->getEnabled());
+
+		$venue->setEnabled(false);
+		$this->assertSame(false, $venue->getEnabled());
+	}
+
+	public function testGetSetHidden() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => true));
+		$this->assertSame(false, $venue->getHidden());
+
+		$venue->setHidden(true);
+		$this->assertSame(true, $venue->getHidden());
+	}
+
+	public function testGetSetSource() {
+		/** @var Denkmal_Model_Venue $venue */
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Example', 'queued' => true, 'enabled' => true, 'source' => 3));
+		$this->assertSame(3, $venue->getSource());
+
+		$venue->setSource(null);
+		$this->assertSame(null, $venue->getSource());
+
+		$venue->setSource(5);
+		$this->assertSame(5, $venue->getSource());
 	}
 }

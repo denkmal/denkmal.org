fc961e0d88618dd1b6570b743fca4b2844001010
Reto Kaiser <reto@cargomedia.ch>
2013-06-09 02:58:43 +0200

    Fix searchForm JS in headerbar

--- library/Denkmal/library/Denkmal/Component/HeaderBar.js
+++ library/Denkmal/library/Denkmal/Component/HeaderBar.js
@@ -7,6 +7,9 @@ var Denkmal_Component_HeaderBar = Denkmal_Component_Abstract.extend({
 	/** @type String */
 	_class: 'Denkmal_Component_HeaderBar',
 
+	/** @type Denkmal_Form_SearchContent|Null */
+	_searchForm: null,
+
 	events: {
 		'click .showSearch': 'showSearch'
 	},
@@ -25,15 +28,17 @@ var Denkmal_Component_HeaderBar = Denkmal_Component_Abstract.extend({
 			return;
 		}
 		this._expandSearch(true);
+		console.log(this);
+		console.log(this._getSearchForm());
 		this._getSearchForm().setFocus();
 	},
 
 	/**
-	 * @return FB_Form_SearchContent
+	 * @return Denkmal_Form_SearchContent
 	 */
 	_getSearchForm: function() {
 		if (null === this._searchForm) {
-			this._searchForm = this.findChild('FB_Form_SearchContent');
+			this._searchForm = this.findChild('Denkmal_Form_SearchContent');
 		}
 		return this._searchForm;
 	},

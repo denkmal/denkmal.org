b61ead90baaa07033e4f1a8078a5b1893e06eec9
Tomasz Durka <tomasz@durka.pl>
2013-06-09 17:17:46 +0200

    Add mock of LinkList component

---
+++ library/Admin/library/Admin/Component/LinkList/All.php
@@ -0,0 +1,10 @@
+<?php
+
+class Admin_Component_LinkList_All extends Admin_Component_Abstract {
+
+	public function prepare() {
+		$links = new Denkmal_Paging_Link_All();
+
+		$this->setTplParam('links', $links);
+	}
+}

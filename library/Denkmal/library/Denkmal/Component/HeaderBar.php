2eb7b723bfb11ae6c3b29daa3b4959f0d4a8f640
Reto Kaiser <reto@cargomedia.ch>
2013-06-09 02:59:59 +0200

    Use menu for date links

--- library/Denkmal/library/Denkmal/Component/HeaderBar.php
+++ library/Denkmal/library/Denkmal/Component/HeaderBar.php
@@ -3,15 +3,20 @@
 class Denkmal_Component_HeaderBar extends Denkmal_Component_Abstract {
 
 	public function prepare() {
+		$dateMenu = array();
 		$date = new Denkmal_Date();
 		$dateInterval = new DateInterval('P1D');
-		$dateList = array($date);
 		for ($i = 1; $i < 7; $i++) {
 			$date = clone $date;
 			$date->add($dateInterval);
-			$dateList[] = $date;
+			$dateMenu[] = array(
+				'label'  => $date->getWeekday(),
+				'page'   => 'Denkmal_Page_Events',
+				'params' => array('date' => $date->__toString()),
+				'class'  => 'navButton',
+			);
 		}
 
-		$this->setTplParam('dateList', $dateList);
+		$this->setTplParam('dateMenu', $dateMenu);
 	}
 }

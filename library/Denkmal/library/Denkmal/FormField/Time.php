cd711cd3e5cc68f8aff650c33247b18bea0e2e60
Tomasz Durka <tomasz@durka.pl>
2013-06-09 02:33:53 +0200

    Add Denkmal time

---
+++ library/Denkmal/library/Denkmal/FormField/Time.php
@@ -0,0 +1,17 @@
+<?php
+
+class Denkmal_FormField_Time extends CM_FormField_Text {
+
+	public function validate($userInput, CM_Response_Abstract $response) {
+		$userInput = parent::validate($userInput, $response);
+		if (!preg_match('/^(\d{1,2})(?::(\d{2}))?$/', $userInput, $matches)) {
+			throw new CM_Exception_FormFieldValidation('Invalid time');
+		}
+		$hour = (int) $matches[1];
+		$minute = array_key_exists(2, $matches) ? $matches[2] : 0;
+		if ($hour > 24 || $minute > 60 || ($hour === 24 && $minute > 0)) {
+			throw new CM_Exception_FormFieldValidation('Invalid time');
+		}
+		return array('hour' => $hour, 'minute' => $minute);
+	}
+}

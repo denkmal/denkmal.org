589c8e4260ef983d5b821b83622762547b8f6b9c
Reto Kaiser <reto@cargomedia.ch>
2013-06-09 19:19:04 +0200

    Add venue name checks to venue forms

--- library/Admin/library/Admin/FormAction/Venue/Add.php
+++ library/Admin/library/Admin/FormAction/Venue/Add.php
@@ -12,6 +12,13 @@ class Admin_FormAction_Venue_Add extends CM_FormAction_Abstract {
 		parent::setup($form);
 	}
 
+	protected function _checkData(CM_Params $params, CM_Response_View_Form $response, CM_Form_Abstract $form) {
+		$name = $params->getString('name');
+		if ($venue = Denkmal_Model_Venue::findByNameOrAlias($name)) {
+			$response->addError('Name already used by venue `' . $venue->getName() . '`', 'name');
+		}
+	}
+
 	protected function _process(CM_Params $params, CM_Response_View_Form $response, CM_Form_Abstract $form) {
 		$name = $params->getString('name');
 		$url = $params->has('url') ? $params->getString('url') : null;

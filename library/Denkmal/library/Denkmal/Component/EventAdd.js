562bdb5b2854eba68586d3ca916818ab6692833e
Tomasz Durka <tomasz@durka.pl>
2013-06-09 23:21:06 +0200

    Introduce change event bidning for compontent reload

--- library/Denkmal/library/Denkmal/Component/EventAdd.js
+++ library/Denkmal/library/Denkmal/Component/EventAdd.js
@@ -5,5 +5,18 @@
 var Denkmal_Component_EventAdd = Denkmal_Component_Abstract.extend({
 
 	/** @type String */
-	_class: 'Denkmal_Component_EventAdd'
+	_class: 'Denkmal_Component_EventAdd',
+
+	childrenEvents: {
+		'Denkmal_Form_EventAdd change': function() {
+			console.log('change-' + new Date().getTime());
+		}
+	},
+
+	ready: function() {
+		var eventAddForm = this.findChild('Denkmal_Form_EventAdd');
+		_.each(eventAddForm.getChildren('CM_FormField_Text'), function(field) {
+			field.enableTriggerChange();
+		});
+	}
 });

46d87dc719ffeb5b46dd89b20a692c50e94bb92b
Tomasz Durka <tomasz@durka.pl>
2013-06-09 13:48:19 +0200

    Refactor EventAdd form, naming mostly

--- library/Denkmal/library/Denkmal/Form/EventAdd.php
+++ library/Denkmal/library/Denkmal/Form/EventAdd.php
@@ -8,13 +8,13 @@ class Denkmal_Form_EventAdd extends CM_Form_Abstract {
 		$this->registerField(new CM_FormField_Text('venueUrl'));
 
 		$this->registerField(new CM_FormField_Date('date'));
-		$this->registerField(new CM_FormField_Text('fromTime'));
-		$this->registerField(new CM_FormField_Text('untilTime'));
+		$this->registerField(new Denkmal_FormField_Time('fromTime'));
+		$this->registerField(new Denkmal_FormField_Time('untilTime'));
 
 		$this->registerField(new CM_FormField_Text('title'));
-		$this->registerField(new CM_FormField_Text('artist'));
+		$this->registerField(new CM_FormField_Text('artists'));
 		$this->registerField(new CM_FormField_Text('genres'));
-		$this->registerField(new CM_FormField_Text('url'));
+		$this->registerField(new CM_FormField_Text('urls'));
 
 		$this->registerAction(new Denkmal_FormAction_EventAdd_Create());
 	}

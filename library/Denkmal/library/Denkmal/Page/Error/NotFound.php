9706f69527a3c0174b1dcfe9b448df067c471403
Tomasz Durka <tomasz@durka.pl>
2013-06-09 22:44:39 +0200

    Add NotFound page

---
+++ library/Denkmal/library/Denkmal/Page/Error/NotFound.php
@@ -0,0 +1,11 @@
+<?php
+
+class Denkmal_Page_Error_NotFound extends Denkmal_Page_Abstract {
+
+	public function prepareResponse(CM_Response_Page $response) {
+		$response->setHeaderNotfound();
+	}
+
+	public function prepare() {
+	}
+}

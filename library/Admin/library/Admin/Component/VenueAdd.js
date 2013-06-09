65aa49a4675c2647195a582b230cc7d2a46d6d96
Reto Kaiser <reto@cargomedia.ch>
2013-06-09 17:20:43 +0200

    Revert "Split venue add/edit in admin"
    This reverts commit 7f8edaa81c787c077f3b8e7afd676dc3c99fe750.

--- library/Admin/library/Admin/Component/VenueAdd.js
+++
@@ -1,9 +0,0 @@
-/**
- * @class Admin_Component_VenueAdd
- * @extends Admin_Component_Abstract
- */
-var Admin_Component_VenueAdd = Admin_Component_Abstract.extend({
-
-	/** @type String */
-	_class: 'Admin_Component_VenueAdd'
-});

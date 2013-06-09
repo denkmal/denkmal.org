3586b721e104fc6842ae76dd2049e26d4904d192
Tomasz Durka <tomasz@durka.pl>
2013-06-09 14:46:53 +0200

    Implement Paging_Event_Date
    - rename DatePublic -> Date
    - add DateTIme and publicOnly param
    - implement test

---
+++ library/Denkmal/library/Denkmal/Paging/Event/Date.php
@@ -0,0 +1,18 @@
+<?php
+
+class Denkmal_Paging_Event_Date extends Denkmal_Paging_Event_Abstract {
+
+	public function __construct(DateTime $date, $publicOnly = null) {
+		$date->setTime(6, 0, 0);
+		$startStamp = $date->getTimestamp();
+		$date->add(new DateInterval('P1D'));
+		$endStamp = $date->getTimestamp();
+		$where = '`from` >= ' . $startStamp . ' AND `from` < ' . $endStamp;
+		if ($publicOnly) {
+			$where .= ' AND `enabled` = 1 AND `hidden` = 0';
+		}
+		$source = new CM_PagingSource_Sql('id', 'denkmal_event', $where, '`starred`, `id`');
+		$source->enableCache();
+		parent::__construct($source);
+	}
+}
--- tests/library/Denkmal/Paging/Event/DatePublicTest.php
+++ tests/library/Denkmal/Paging/Event/DatePublicTest.php
@@ -1,21 +1,45 @@
 <?php
 
-class Denkmal_Paging_Event_DatePublicTest extends CMTest_TestCase {
+class Denkmal_Paging_Event_DateTest extends CMTest_TestCase {
 
 	public function tearDown() {
 		CMTest_TH::clearEnv();
 	}
 
 	public function testGetItems() {
+		$today = new DateTime();
+		$today->setTime(20, 0, 0);
+		$past = clone $today;
+		$past->sub(new DateInterval('P2D'));
+		$future = clone $today;
+		$future->add(new DateInterval('P2D'));
+
 		$venue = Denkmal_Model_Venue::create(array('name' => 'Foo', 'queued' => false, 'enabled' => true));
 		$event1 = Denkmal_Model_Event::create(
-			array('venue' => $venue, 'description' => 'Foo 1', 'from' => 123, 'queued' => false, 'enabled' => true, 'hidden' => false));
+			array('venue' => $venue, 'description' => 'Foo 1', 'from' => $future->getTimestamp(), 'queued' => false, 'enabled' => true, 'hidden' => false));
 		$event2 = Denkmal_Model_Event::create(
-			array('venue' => $venue, 'description' => 'Foo 2', 'from' => 123, 'queued' => false, 'enabled' => true, 'hidden' => false));
+			array('venue' => $venue, 'description' => 'Foo 2', 'from' => $past->getTimestamp(), 'queued' => false, 'enabled' => true, 'hidden' => false));
 		$event3 = Denkmal_Model_Event::create(
-			array('venue' => $venue, 'description' => 'Foo 3', 'from' => 123, 'queued' => false, 'enabled' => true, 'hidden' => false));
+			array('venue' => $venue, 'description' => 'Foo 3', 'from' => $today->getTimestamp(), 'queued' => false, 'enabled' => true, 'hidden' => false));
 
-		$paging = new Denkmal_Paging_Event_DatePublic();
-		$this->assertEquals(array($event1, $event2, $event3), $paging);
+		$paging = new Denkmal_Paging_Event_Date($today);
+		$this->assertEquals(array($event3), $paging);
+	}
+
+	public function testGetItemsPublic() {
+		$today = new DateTime();
+		$today->setTime(20, 0, 0);
+
+		$venue = Denkmal_Model_Venue::create(array('name' => 'Foo', 'queued' => false, 'enabled' => true));
+		$event1 = Denkmal_Model_Event::create(
+			array('venue' => $venue, 'description' => 'Foo 1', 'from' => $today->getTimestamp(), 'queued' => false, 'enabled' => true, 'hidden' => true));
+		$event2 = Denkmal_Model_Event::create(
+			array('venue' => $venue, 'description' => 'Foo 2', 'from' => $today->getTimestamp(), 'queued' => false, 'enabled' => true, 'hidden' => false));
+		$event3 = Denkmal_Model_Event::create(
+			array('venue' => $venue, 'description' => 'Foo 3', 'from' => $today->getTimestamp(), 'queued' => false, 'enabled' => false, 'hidden' => false));
+
+		$paging = new Denkmal_Paging_Event_Date($today, true);
+		$this->assertEquals(array($event2), $paging);
+
 	}
 }

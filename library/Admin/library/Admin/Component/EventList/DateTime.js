d6e021ec71cbf77820a97a4bd96b131a16474acf
Reto Kaiser <reto@cargomedia.ch>
2013-06-09 17:04:19 +0200

    Add paging of events per date

---
+++ library/Admin/library/Admin/Component/EventList/DateTime.js
@@ -0,0 +1,9 @@
+/**
+ * @class Admin_Component_EventList_DateTime
+ * @extends Admin_Component_EventList_Abstract
+ */
+var Admin_Component_EventList_DateTime = Admin_Component_EventList_Abstract.extend({
+
+	/** @type String */
+	_class: 'Admin_Component_EventList_DateTime'
+});

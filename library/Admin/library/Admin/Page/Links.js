f51c8c4d3bcfd47449cadce4aad38ff9ca0bb5ee
Tomasz Durka <tomasz@durka.pl>
2013-06-09 17:11:03 +0200

    Add Links page to Admin

---
+++ library/Admin/library/Admin/Page/Links.js
@@ -0,0 +1,9 @@
+/**
+ * @class Admin_Page_Links
+ * @extends Admin_Page_Abstract
+ */
+var Admin_Page_Links = Admin_Page_Abstract.extend({
+
+	/** @type String */
+	_class: 'Admin_Page_Links'
+});
---
+++ library/Admin/library/Admin/Page/Links.php
@@ -0,0 +1,5 @@
+<?php
+
+class Admin_Page_Links extends Admin_Page_Abstract {
+
+}

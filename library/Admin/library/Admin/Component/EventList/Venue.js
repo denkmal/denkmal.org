577fa451af2aa2fb1e4a1a5c1504ed6dd2292c32
Reto Kaiser <reto@cargomedia.ch>
2013-06-09 16:24:01 +0200

    Add admin event list for venue

---
+++ library/Admin/library/Admin/Component/EventList/Venue.js
@@ -0,0 +1,9 @@
+/**
+ * @class Admin_Component_EventList_Venue
+ * @extends Admin_Component_EventList_Abstract
+ */
+var Admin_Component_EventList_Venue = Admin_Component_EventList_Abstract.extend({
+
+	/** @type String */
+	_class: 'Admin_Component_EventList_Venue'
+});

641206da0917c1b88652a9db238777a27c77a2b1
Reto Kaiser <reto@cargomedia.ch>
2013-06-16 13:39:50 +0200

    Check for message model in api response

--- tests/library/Denkmal/Response/Api/MessageTest.php
+++ tests/library/Denkmal/Response/Api/MessageTest.php
@@ -23,5 +23,13 @@ class Denkmal_Response_Api_MessageTest extends CMTest_TestCase {
 		$request = new CM_Request_Post('/api/message', array('host' => 'denkmal.test'), $body);
 		$response = new Denkmal_Response_Api_Message($request);
 		$response->process();
+
+		$messageList = new Denkmal_Paging_Message_All();
+		$this->assertCount(1, $messageList);
+		/** @var Denkmal_Model_Message $message */
+		$message = $messageList->getItem(0);
+		$this->assertEquals($venue, $message->getVenue());
+		$this->assertSame('hallo test', $message->getText());
+		$this->assertSameTime(time(), $message->getCreated());
 	}
 }

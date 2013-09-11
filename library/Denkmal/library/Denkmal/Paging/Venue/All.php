ce850256c58b8f1ad99c769c266bde3ac561951b
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-09-11 15:40:30 +0200

    Updated table name in paging

--- library/Denkmal/library/Denkmal/Paging/Venue/All.php
+++ library/Denkmal/library/Denkmal/Paging/Venue/All.php
@@ -3,7 +3,7 @@
 class Denkmal_Paging_Venue_All extends Denkmal_Paging_Venue_Abstract {
 
 	public function __construct() {
-		$source = new CM_PagingSource_Sql('id', 'denkmal_venue', null, '`name`');
+		$source = new CM_PagingSource_Sql('id', 'denkmal_model_venue', null, '`name`');
 		$source->enableCache();
 		parent::__construct($source);
 	}

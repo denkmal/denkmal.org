b407ba36bd124b8ccf1cfebf403e10a9dcb32166
Tomasz Durka <tomasz@durka.pl>
2013-09-11 17:45:16 +0200

    Dont pass null for song

--- library/Denkmal/library/Denkmal/FormAction/EventAdd/Create.php
+++ library/Denkmal/library/Denkmal/FormAction/EventAdd/Create.php
@@ -55,6 +55,6 @@ class Denkmal_FormAction_EventAdd_Create extends CM_FormAction_Abstract {
 			$description = $title;
 			$title = null;
 		}
-		Denkmal_Model_Event::create($venue, $description, false, true, $from, $until, $title, null);
+		Denkmal_Model_Event::create($venue, $description, false, true, $from, $until, $title);
 	}
 }

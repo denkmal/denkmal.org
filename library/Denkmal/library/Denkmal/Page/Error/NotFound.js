5a1735e9698bb91e04d53db1cf9d10c76d10b2e7
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-28 01:06:58 +0200

    Add JS for notfound page

---
+++ library/Denkmal/library/Denkmal/Page/Error/NotFound.js
@@ -0,0 +1,9 @@
+/**
+ * @class Denkmal_Page_Error_NotFound
+ * @extends Denkmal_Page_Abstract
+ */
+var Denkmal_Page_Error_NotFound = Denkmal_Page_Abstract.extend({
+
+	/** @type String */
+	_class: 'Denkmal_Page_Error_NotFound'
+});

18959857d9e576dc641ddf884676d955d1312854
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-28 00:55:31 +0200

    Set default label on song upload from filename

--- library/Admin/library/Admin/Form/Song.js
+++ library/Admin/library/Admin/Form/Song.js
@@ -3,5 +3,19 @@
  * @extends CM_Form_Abstract
  */
 var Admin_Form_Song = CM_Form_Abstract.extend({
-	_class: 'Admin_Form_Song'
+	_class: 'Admin_Form_Song',
+
+	childrenEvents: {
+		'Denkmal_FormField_FileSong uploadComplete': function(field, files) {
+			this._setLabelFromFilename(files[0].name);
+		}
+	},
+
+	/**
+	 * @param {String} filename
+	 */
+	_setLabelFromFilename: function(filename) {
+		filename = filename.replace(/\.[^\.]+$/, '');
+		this.getField('label').setValue(filename);
+	}
 });

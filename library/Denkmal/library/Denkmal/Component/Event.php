ac4a79bbeeaee29d6bca34c54d7bfeba1591ddb7
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-27 15:09:22 +0200

    Use actual event model for form preview

--- library/Denkmal/library/Denkmal/Component/Event.php
+++ library/Denkmal/library/Denkmal/Component/Event.php
@@ -3,34 +3,12 @@
 class Denkmal_Component_Event extends Denkmal_Component_Abstract {
 
 	public function prepare() {
-		if ($this->_params->has('event') && $this->_params->has('data')) {
-			throw new CM_Exception_Invalid('Can\'t pass both `event` and `data`');
-		}
+		$event = $this->_params->getEvent('event');
+		$venue = $this->_params->has('venue') ? $this->_params->getVenue('venue') : $event->getVenue();
 
-		$data = null;
-		if ($this->_params->has('event')) {
-			$event = $this->_params->getEvent('event');
-			$venue = $event->getVenue();
-			$data = array(
-				'description' => $event->getDescription(),
-				'title'       => $event->getTitle(),
-				'from'        => $event->getFrom()->format('H:m'),
-				'starred'     => $event->getStarred(),
+		$this->setTplParam('event', $event);
+		$this->setTplParam('venue', $venue);
 
-				'venue'       => $venue->getName(),
-				'address'     => $venue->getAddress(),
-				'url'         => $venue->getAddress(),
-			);
-		}
-		if ($this->_params->has('data')) {
-			$data = $this->_params->getArray('data');
-			if (is_numeric($data['venue'])) {
-				$venue = new Denkmal_Model_Venue($data['venue']);
-				$data['venue'] = $venue->getName();
-				$data['address'] = $venue->getAddress();
-				$data['url'] = $venue->getUrl();
-			}
-		}
-		$this->setTplParam('data', $data);
+		$this->_params = CM_Params::factory(); // Empty params to not send them to client
 	}
 }

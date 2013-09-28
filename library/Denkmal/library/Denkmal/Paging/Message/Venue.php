c4aa2d9a7b8e31aad4fde6b551eceeeee6d8f4cb
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-09-28 18:57:07 +0200

    Added Denkmal_Paging_Message_Venue

---
+++ library/Denkmal/library/Denkmal/Paging/Message/Venue.php
@@ -0,0 +1,14 @@
+<?php
+
+class Denkmal_Paging_Message_Venue extends Denkmal_Paging_Message_Abstract {
+
+	/**
+	 * @param Denkmal_Model_Venue $venue
+	 */
+	public function __construct(Denkmal_Model_Venue $venue) {
+		$where = '`venue` = ' . $venue->getId();
+		$source = new CM_PagingSource_Sql('id', 'denkmal_model_message', $where, '`created`');
+		$source->enableCache();
+		parent::__construct($source);
+	}
+}

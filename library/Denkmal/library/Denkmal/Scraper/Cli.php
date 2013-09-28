527b9a0957fa1f711d446e9f053298736aff3810
Reto Kaiser <reto@cargomedia.ch>
2013-09-28 19:10:06 +0200

    Add scraper sceleton, add CLI class

---
+++ library/Denkmal/library/Denkmal/Scraper/Cli.php
@@ -0,0 +1,15 @@
+<?php
+
+class Denkmal_Scraper_Cli extends CM_Cli_Runnable_Abstract {
+
+	public function run() {
+		foreach (Denkmal_Scraper_Source_Abstract::getAll() as $scraper) {
+			$this->_getOutput()->writeln('Running scraper `' . get_class($scraper) . '`…');
+			$scraper->run();
+		}
+	}
+
+	public static function getPackageName() {
+		return 'scraper';
+	}
+}
---
+++ library/Denkmal/library/Denkmal/Scraper/Source/Abstract.php
@@ -0,0 +1,50 @@
+<?php
+
+abstract class Denkmal_Scraper_Source_Abstract extends CM_Class_Abstract {
+
+	abstract public function run();
+
+	/**
+	 * @return DateTime[]
+	 */
+	protected function _getDateList() {
+		$day = new DateInterval('P1D');
+		$dayCount = (int) self::_getConfig()->dayCount;
+		$dateList = array();
+		$date = new DateTime();
+		$date->setTime(0, 0, 0);
+		for ($i = 0; $i < $dayCount; $i++) {
+			$dateList[] = clone $date;
+			$date->add($day);
+		}
+		return $dateList;
+	}
+
+	/**
+	 * @param Denkmal_Model_Venue|string $venue        Location-name
+	 * @param string                     $description  Description
+	 * @param DateTime                   $from         From-date
+	 * @param DateTime|null              $until        Until-date
+	 */
+	protected function _addEvent($venue, $description, DateTime $from, DateTime $until = null) {
+		if (is_string($venue)) {
+			$venue = Denkmal_Model_Venue::findByNameOrAlias($venue);
+			if (null === $venue) {
+				$venue = Denkmal_Model_Venue::create($venue, true, false, false);
+			}
+		}
+		// @todo
+		//		$this->__addEvent($venue, $description, $from, $until);
+	}
+
+	/**
+	 * @return Denkmal_Scraper_Source_Abstract[]
+	 */
+	public static function getAll() {
+		$scraperList = array();
+		foreach (CM_Util::getClassChildren('Denkmal_Scraper_Source_Abstract') as $className) {
+			$scraperList[] = new $className;
+		}
+		return $scraperList;
+	}
+}

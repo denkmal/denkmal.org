efd1a48911154ce3e714fe5640c1c35a6c52b7e6
Reto Kaiser <reto@cargomedia.ch>
2013-09-29 00:58:12 +0200

    Add message action, stream CREATE to message streamChannel

---
+++ library/Denkmal/library/Denkmal/Action/Message.php
@@ -0,0 +1,19 @@
+<?php
+
+class Denkmal_Action_Message extends Denkmal_Action_Abstract {
+
+	const TYPE = 100;
+
+	public function notify(Denkmal_Model_Message $message) {
+		$this->_notify($message);
+	}
+
+	protected function _notifyCreate(Denkmal_Model_Message $message) {
+		CM_Model_StreamChannel_Message::publish('messages', 'create', array(
+			'id'      => $message->getId(),
+			'venue'   => $message->getVenue()->getId(),
+			'created' => $message->getCreated()->getTimestamp(),
+			'text'    => $message->getText(),
+		));
+	}
+}

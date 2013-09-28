3b4de31d9655a69661c3d0376b96679af1c19638
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-28 16:58:38 +0200

    add admin pagings

--- library/Admin/library/Admin/Paging/Event/Date.php
+++ library/Admin/library/Admin/Paging/Event/Date.php
@@ -2,19 +2,13 @@
 
 class Admin_Paging_Event_Date extends Denkmal_Paging_Event_Abstract {
 
-	public function __construct(DateTime $date, $queueOnly = null) {
+	public function __construct(DateTime $date) {
 		$date = clone $date;
 		$date->setTime(6, 0, 0);
 		$startStamp = $date->getTimestamp();
 		$date->add(new DateInterval('P1D'));
 		$endStamp = $date->getTimestamp();
-		$where = '`from` >= ' . $startStamp . ' AND `from` < ' . $endStamp;
-
-		if ($queueOnly) {
-			$where .= ' AND `queued` = 1';
-		} else {
-			$where .= ' AND `enabled` = 1';
-		}
+		$where = '`from` >= ' . $startStamp . ' AND `from` < ' . $endStamp . ' AND `enabled` = 1';
 
 		$source = new CM_PagingSource_Sql('id', 'denkmal_model_event', $where, '`starred`, `id`');
 		parent::__construct($source);

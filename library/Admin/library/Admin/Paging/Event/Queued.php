2e3694e860342ed76ab4f303d632ccb16f6bc911
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-28 19:33:52 +0200

    remove hidden events from eventListQueue

--- library/Admin/library/Admin/Paging/Event/Queued.php
+++ library/Admin/library/Admin/Paging/Event/Queued.php
@@ -3,7 +3,7 @@
 class Admin_Paging_Event_Queued extends Denkmal_Paging_Event_Abstract {
 
 	public function __construct() {
-		$where = '`queued` = 1';
+		$where = '`queued` = 1 AND `hidden` = 0';
 
 		$source = new CM_PagingSource_Sql('id', 'denkmal_model_event', $where, '`from`');
 		parent::__construct($source);

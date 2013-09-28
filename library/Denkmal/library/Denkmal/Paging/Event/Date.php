64082a2fab055642cd8b608aeab49cc09a107b5b
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-28 20:03:17 +0200

    add bool to docu

--- library/Denkmal/library/Denkmal/Paging/Event/Date.php
+++ library/Denkmal/library/Denkmal/Paging/Event/Date.php
@@ -3,8 +3,8 @@
 class Denkmal_Paging_Event_Date extends Denkmal_Paging_Event_Abstract {
 
 	/**
-	 * @param DateTime $date
-	 * @param null     $showAll
+	 * @param DateTime  $date
+	 * @param bool|null $showAll
 	 */
 	public function __construct(DateTime $date, $showAll = null) {
 		$date = clone $date;
@@ -15,7 +15,7 @@ class Denkmal_Paging_Event_Date extends Denkmal_Paging_Event_Abstract {
 		$where = '`from` >= ' . $startStamp . ' AND `from` < ' . $endStamp;
 
 		if (!$showAll) {
-			$where .=  ' AND `enabled` = 1 AND `hidden` = 0';
+			$where .= ' AND `enabled` = 1 AND `hidden` = 0';
 		}
 
 		$source = new CM_PagingSource_Sql('id', 'denkmal_model_event', $where, '`starred`, `id`');

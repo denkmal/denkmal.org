a8cec5b9d1451b0a00770af3e04dc0c4650bcb33
Reto Kaiser <reto@cargomedia.ch>
2013-09-28 23:06:01 +0200

    Add /error/not-found page in admin

---
+++ library/Admin/library/Admin/Page/Error/NotFound.php
@@ -0,0 +1,11 @@
+<?php
+
+class Admin_Page_Error_NotFound extends Admin_Page_Abstract {
+
+	public function prepareResponse(CM_Response_Page $response) {
+		$response->setHeaderNotfound();
+	}
+
+	public function prepare() {
+	}
+}

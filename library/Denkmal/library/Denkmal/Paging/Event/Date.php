7214f99f903eb937844a89328092e412c0fdab60
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-28 19:33:17 +0200

    remove admin paging date:
    - introduce a flag on denkmal paging date

--- library/Denkmal/library/Denkmal/Paging/Event/Date.php
+++ library/Denkmal/library/Denkmal/Paging/Event/Date.php
@@ -2,13 +2,17 @@
 
 class Denkmal_Paging_Event_Date extends Denkmal_Paging_Event_Abstract {
 
-	public function __construct(DateTime $date) {
+	public function __construct(DateTime $date, $showAll = null) {
 		$date = clone $date;
 		$date->setTime(6, 0, 0);
 		$startStamp = $date->getTimestamp();
 		$date->add(new DateInterval('P1D'));
 		$endStamp = $date->getTimestamp();
-		$where = '`from` >= ' . $startStamp . ' AND `from` < ' . $endStamp . ' AND `enabled` = 1 AND `hidden` = 0';
+		$where = '`from` >= ' . $startStamp . ' AND `from` < ' . $endStamp;
+
+		if (!$showAll) {
+			$where .=  ' AND `enabled` = 1 AND `hidden` = 0';
+		}
 
 		$source = new CM_PagingSource_Sql('id', 'denkmal_model_event', $where, '`starred`, `id`');
 		parent::__construct($source);

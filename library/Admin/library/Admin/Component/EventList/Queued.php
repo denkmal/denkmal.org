1e3bf65ad4ff8abaeda6cf9e7d1c08852a34a15d
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-09-28 19:11:08 +0200

    Admin_Component_VenueList_Queued: Remove date-passing to paging Admin_Component_EventList_Queued: Remove date-passing to paging Admin_Component_VenueList_Queued: Remove comment "change"?

--- library/Admin/library/Admin/Component/EventList/Queued.php
+++ library/Admin/library/Admin/Component/EventList/Queued.php
@@ -4,7 +4,6 @@ class Admin_Component_EventList_Queued extends Admin_Component_EventList_Abstrac
 
 	public function prepare() {
 		$params = $this->getParams();
-		$date = $params->getDateTime('date');
 		$eventList = new Admin_Paging_Event_Queued();
 
 		$this->_prepareList($eventList);
--- library/Admin/library/Admin/Component/VenueList/Queued.php
+++ library/Admin/library/Admin/Component/VenueList/Queued.php
@@ -3,7 +3,7 @@
 class Admin_Component_VenueList_Queued extends Admin_Component_VenueList_Abstract {
 
 	public function prepare() {
-		$venueList = new Admin_Paging_Venue_Queued(); // change
+		$venueList = new Admin_Paging_Venue_Queued();
 
 		$this->setTplParam('venueList', $venueList);
 	}

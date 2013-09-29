40699c0a7663bdaebd7813fa6e0040a60495c18c
Reto Kaiser <reto@cargomedia.ch>
2013-09-29 02:32:40 +0200

    Add test for description scraper with genres

--- tests/library/Denkmal/Scraper/DescriptionTest.php
+++ tests/library/Denkmal/Scraper/DescriptionTest.php
@@ -7,4 +7,11 @@ class Denkmal_Scraper_DescriptionTest extends CMTest_TestCase {
 
 		$this->assertSame('Foo bar', $description->getDescriptionAndGenres());
 	}
+
+	public function testGetDescriptionAndGenresWithGenres() {
+		$genres = new Denkmal_Scraper_Genres('rock, rap');
+		$description = new Denkmal_Scraper_Description('foo bar', null, $genres);
+
+		$this->assertSame('Foo bar. Rock, rap', $description->getDescriptionAndGenres());
+	}
 }

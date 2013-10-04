ef86d196ff4c3abc6f0276e71eaf9eb833d3ee13
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-04 17:34:16 +0200

    add delete to Admin_Component_SongList_Abstract

--- library/Admin/layout/default/Component/SongList_Abstract/default.tpl
+++ library/Admin/layout/default/Component/SongList_Abstract/default.tpl
@@ -1,7 +1,8 @@
 <ul class="songList">
 	{foreach $songList as $song}
-		<li class="song">
+		<li class="song" data-id="{$song->getId()}">
 			{$song->getLabel()|escape}
+			{link icon="delete" class="deleteSong"}
 		</li>
 	{/foreach}
 </ul>
--- library/Admin/library/Admin/Component/SongList/Abstract.js
+++ library/Admin/library/Admin/Component/SongList/Abstract.js
@@ -5,5 +5,20 @@
 var Admin_Component_SongList_Abstract = Admin_Component_Abstract.extend({
 
 	/** @type String */
-	_class: 'Admin_Component_SongList_Abstract'
+	_class: 'Admin_Component_SongList_Abstract',
+
+	events: {
+		'clickConfirmed .deleteSong': function(event) {
+			var id = $(event.currentTarget).closest('li.song').data('id');
+			this.deleteSong(id);
+			return false;
+		}
+	},
+
+	/**
+	 * @param {Number} id
+	 */
+	deleteSong: function(id) {
+		this.ajax('deleteSong', {id: id});
+	}
 });

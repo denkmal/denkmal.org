f6824888b2da3abec0f65734dc3e24b650f06a9f
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-04 19:00:44 +0200

    add pagination

--- library/Admin/library/Admin/Component/LinkList.php
+++ library/Admin/library/Admin/Component/LinkList.php
@@ -4,6 +4,7 @@ class Admin_Component_LinkList extends Admin_Component_Abstract {
 
 	public function prepare() {
 		$linkList = new Denkmal_Paging_Link_All();
+		$linkList->setPage($this->_params->getPage(), $this->_params->getInt('count', 30));
 
 		$this->setTplParam('linkList', $linkList);
 	}

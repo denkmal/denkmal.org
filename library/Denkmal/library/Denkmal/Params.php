cc0795a9048dea40291b338f3285b568f5cc60ef
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-04 18:33:06 +0200

    add link to denkmal params

--- library/Denkmal/library/Denkmal/Params.php
+++ library/Denkmal/library/Denkmal/Params.php
@@ -31,6 +31,15 @@ class Denkmal_Params extends CM_Params {
 
 	/**
 	 * @param string $key
+	 * @return Denkmal_Model_Link
+	 * @throws CM_Exception_InvalidParam
+	 */
+	public function getLink($key) {
+		return $this->_getObject($key, 'Denkmal_Model_Link');
+	}
+
+	/**
+	 * @param string $key
 	 * @return Denkmal_Model_Song
 	 * @throws CM_Exception_InvalidParam
 	 */

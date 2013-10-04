98ac9993e4361cd269c69861e577316fc20f5228
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-04 19:01:13 +0200

    reduce paging on queued list

--- library/Admin/library/Admin/Component/EventList/Queued.php
+++ library/Admin/library/Admin/Component/EventList/Queued.php
@@ -3,8 +3,8 @@
 class Admin_Component_EventList_Queued extends Admin_Component_EventList_Abstract {
 
 	public function prepare() {
-		$params = $this->getParams();
 		$eventList = new Admin_Paging_Event_Queued();
+		$eventList->setPage($this->_params->getPage(), $this->_params->getInt('count', 10));
 
 		$this->_prepareList($eventList);
 	}

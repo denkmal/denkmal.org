44c488b9aac08f5ac0163d0e5e7ffed5240bc997
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-11 11:16:15 +0200

    add cmp Admin_Component_Filter_Search

---
+++ library/Admin/layout/default/Component/Filter_Search/default.tpl
@@ -0,0 +1,3 @@
+{form name='Admin_Form_Search' searchTerm=$searchTerm urlPage=$urlPage}
+	{formField name='searchTerm' prepend={button action='Process' label={translate 'Suchen'}}}
+{/form}
---
+++ library/Admin/library/Admin/Component/Filter/Search.php
@@ -0,0 +1,12 @@
+<?php
+
+class Admin_Component_Filter_Search extends Admin_Component_Abstract {
+
+	public function prepare() {
+		$urlPage = $this->_params->getString('urlPage');
+		$searchTerm = $this->_params->has('searchTerm') ? $this->_params->getString('searchTerm') : null;
+
+		$this->setTplParam('urlPage', $urlPage);
+		$this->setTplParam('searchTerm', $searchTerm);
+	}
+}

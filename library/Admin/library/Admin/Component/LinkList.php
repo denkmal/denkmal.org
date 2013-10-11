38c73851c4146cf72e52f713e1df22246529e908
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-11 11:17:41 +0200

    adjust lists to handle new params

--- library/Admin/library/Admin/Component/LinkList.php
+++ library/Admin/library/Admin/Component/LinkList.php
@@ -3,9 +3,16 @@
 class Admin_Component_LinkList extends Admin_Component_Abstract {
 
 	public function prepare() {
-		$linkList = new Denkmal_Paging_Link_All();
-		$linkList->setPage($this->_params->getPage(), $this->_params->getInt('count', 30));
+		$searchTerm = $this->_params->has('searchTerm') ? $this->_params->getString('searchTerm') : null;
+
+		if (null !== $searchTerm) {
+			$linkList = new Denkmal_Paging_Link_Search($searchTerm);
+		} else {
+			$linkList = new Denkmal_Paging_Link_All();
+		}
+		$linkList->setPage($this->_params->getPage(), $this->_params->getInt('count', 3));
 
 		$this->setTplParam('linkList', $linkList);
+		$this->setTplParam('searchTerm', $searchTerm);
 	}
 }

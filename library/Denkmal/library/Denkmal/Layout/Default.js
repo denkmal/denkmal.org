7766728453f6421c6e4151f8cd7504ca8d5fa775
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2013-10-11 16:00:20 +0200

    hide menu active triangle when scrolling down

--- library/Denkmal/library/Denkmal/Layout/Default.js
+++ library/Denkmal/library/Denkmal/Layout/Default.js
@@ -5,5 +5,27 @@
 var Denkmal_Layout_Default = CM_Layout_Abstract.extend({
 
 	/** @type String */
-	_class: 'Denkmal_Layout_Default'
+	_class: 'Denkmal_Layout_Default',
+
+	childrenEvents: {
+		'Denkmal_Page_Events ready': function(view) {
+
+			var $layout = this.$el;
+			var $pageScrollables = view.$('.scrollable');
+
+			var onScroll = function() {
+				var scrolledNotTop = view.$('.active .scrollable').scrollTop() > 0;
+				$layout.toggleClass('scrolledNotTop', scrolledNotTop);
+			};
+
+			$pageScrollables.bind('scroll', onScroll);
+			view.on('destruct', function() {
+				$pageScrollables.unbind('scroll', onScroll);
+			});
+			view.on('swipe', function($element) {
+				onScroll();
+			});
+			onScroll();
+		}
+	}
 });

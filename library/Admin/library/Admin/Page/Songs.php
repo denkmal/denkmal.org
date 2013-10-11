6cf899d4879eeae9617c8007d6a16a414181228b
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-11 11:18:28 +0200

    handle search param in pages

--- library/Admin/library/Admin/Page/Songs.php
+++ library/Admin/library/Admin/Page/Songs.php
@@ -2,4 +2,12 @@
 
 class Admin_Page_Songs extends Admin_Page_Abstract {
 
+	public function prepare() {
+		$searchTerm = $this->_params->has('searchTerm') ? $this->_params->getString('searchTerm') : null;
+		if (!$searchTerm) {
+			$searchTerm = null;
+		}
+
+		$this->setTplParam('searchTerm', $searchTerm);
+	}
 }

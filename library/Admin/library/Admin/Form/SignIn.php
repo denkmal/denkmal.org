0671823a80e14b6160272e5b689cab7bb3085976
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-11 19:15:23 +0200

    changed form field to password

--- library/Admin/library/Admin/Form/SignIn.php
+++ library/Admin/library/Admin/Form/SignIn.php
@@ -4,7 +4,7 @@ class Admin_Form_SignIn extends CM_Form_Abstract {
 
 	public function setup() {
 		$this->registerField('login', new CM_FormField_Text());
-		$this->registerField('password', new CM_FormField_Text());
+		$this->registerField('password', new CM_FormField_Password());
 
 		$this->registerAction(new Admin_FormAction_SignIn_Process($this));
 	}

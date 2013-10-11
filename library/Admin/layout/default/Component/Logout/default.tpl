30ad2d7fc28356904f3a5d59c138cb87de7510d2
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-11 18:46:40 +0200

    add cmp logout

---
+++ library/Admin/layout/default/Component/Logout/default.tpl
@@ -0,0 +1,3 @@
+{form name='Admin_Form_Logout'}
+	{button action='Process' label="{translate 'Logout'}" icon="logout"}
+{/form}
---
+++ library/Admin/library/Admin/Component/Logout.js
@@ -0,0 +1,9 @@
+/**
+ * @class Admin_Component_Logout
+ * @extends Admin_Component_Abstract
+ */
+var Admin_Component_Logout = Admin_Component_Abstract.extend({
+
+	/** @type String */
+	_class: 'Admin_Component_Logout'
+});

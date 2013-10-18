0f614ff048800fc266a99a74573fbce4dd9ec722
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-18 20:04:59 +0200

    add broken link list

---
+++ library/Admin/library/Admin/Component/LinkList/Broken.js
@@ -0,0 +1,9 @@
+/**
+ * @class Admin_Component_LinkList_Broken
+ * @extends Admin_Component_LinkList_Abstract
+ */
+var Admin_Component_LinkList_Broken = Admin_Component_LinkList_Abstract.extend({
+
+	/** @type String */
+	_class: 'Admin_Component_LinkList_Broken'
+});

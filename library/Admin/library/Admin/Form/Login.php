c67b5e5b43add5555dedfed8845f033d12d52c03
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-18 10:57:57 +0200

    Use "email" formField

--- library/Admin/library/Admin/Form/Login.php
+++ library/Admin/library/Admin/Form/Login.php
@@ -3,7 +3,7 @@
 class Admin_Form_Login extends CM_Form_Abstract {
 
 	public function setup() {
-		$this->registerField('email', new CM_FormField_Text());
+		$this->registerField('email', new CM_FormField_Email());
 		$this->registerField('password', new CM_FormField_Password());
 
 		$this->registerAction(new Admin_FormAction_Login_Process($this));

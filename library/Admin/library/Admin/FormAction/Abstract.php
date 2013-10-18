ac8358afa2e345cf288535f6c91ffd98ef212151
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-10-18 10:09:43 +0200

    add Admin_FormAction_Abstract with accessible check

---
+++ library/Admin/library/Admin/FormAction/Abstract.php
@@ -0,0 +1,10 @@
+<?php
+
+abstract class Admin_FormAction_Abstract extends Admin_FormAction_Abstract {
+
+	protected function _checkData(CM_Params $params, CM_Response_View_Form $response, CM_Form_Abstract $form) {
+		if (!$response->getViewer(true)->getRoles()->contains(Denkmal_Role::ADMIN)) {
+			$response->addError($response->getRender()->getTranslation('Not Allowed'));
+		}
+	}
+}

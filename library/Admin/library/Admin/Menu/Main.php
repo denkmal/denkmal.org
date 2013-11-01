721f6e1116d318736356e5d0389e36e812a7452f
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2013-11-01 17:25:25 +0100

    add Events page to menu, fix Index page redirect, styles

--- library/Admin/library/Admin/Menu/Main.php
+++ library/Admin/library/Admin/Menu/Main.php
@@ -4,7 +4,7 @@ class Admin_Menu_Main extends CM_Menu {
 
 	public function __construct() {
 		parent::__construct(array(
-			array('label' => 'Veranstaltungen', 'page' => 'Admin_Page_Index'),
+			array('label' => 'Veranstaltungen', 'page' => 'Admin_Page_Events'),
 			array('label' => 'Orte', 'page' => 'Admin_Page_Venues'),
 			array('label' => 'Links', 'page' => 'Admin_Page_Links'),
 			array('label' => 'Lieder', 'page' => 'Admin_Page_Songs'),
--- library/Admin/library/Admin/Page/Events.php
+++ library/Admin/library/Admin/Page/Events.php
@@ -2,6 +2,13 @@
 
 class Admin_Page_Events extends Admin_Page_Abstract {
 
+	public function prepareResponse(CM_Response_Page $response) {
+		if (!$this->_params->has('date')) {
+			$now = Denkmal_Site::getCurrentDate();
+			$response->redirect('Admin_Page_Events', array('date' => $now->format('Y-n-j')));
+		}
+	}
+
 	public function prepare() {
 		$date = $this->_params->getDate('date');
 

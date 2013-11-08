a440eaa48552db998d1a52384e022f3604c05a4b
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-11-08 16:16:18 +0100

    changed order in list

--- library/Admin/library/Admin/Paging/Venue/Queued.php
+++ library/Admin/library/Admin/Paging/Venue/Queued.php
@@ -5,7 +5,7 @@ class Admin_Paging_Venue_Queued extends Denkmal_Paging_Venue_Abstract {
 	public function __construct() {
 		$where = '`queued` = 1';
 
-		$source = new CM_PagingSource_Sql('id', 'denkmal_model_venue', $where);
+		$source = new CM_PagingSource_Sql('id', 'denkmal_model_venue', $where, '`name`');
 		parent::__construct($source);
 	}
 }

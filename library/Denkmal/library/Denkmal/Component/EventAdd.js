d693616398af338786b3c5a538cadee4baa887ae
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-11-15 12:19:17 +0100

    add thank you directly in add cmp

--- library/Denkmal/library/Denkmal/Component/EventAdd.js
+++ library/Denkmal/library/Denkmal/Component/EventAdd.js
@@ -5,5 +5,21 @@
 var Denkmal_Component_EventAdd = Denkmal_Component_Abstract.extend({
 
 	/** @type String */
-	_class: 'Denkmal_Component_EventAdd'
+	_class: 'Denkmal_Component_EventAdd',
+
+	events:{
+		'click .addSimilar': 'addSimilar'
+	},
+
+	childrenEvents: {
+		'Denkmal_Form_EventAdd success': function() {
+			this.$('.Denkmal_Form_EventAdd .formWrapper').slideUp();
+			this.$('.thankYou').slideDown();
+		}
+	},
+
+	addSimilar: function(){
+		this.$('.Denkmal_Form_EventAdd .formWrapper').slideDown();
+		this.$('.thankYou').slideUp();
+	}
 });

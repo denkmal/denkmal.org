16a99f4418c6d0c623b5994b5824745e53c7366f
Reto Kaiser <reto@cargomedia.ch>
2013-12-31 14:50:54 +0100

    Enable cm::application debug mode, include cm::services

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,9 +2,12 @@ node default {
 
   $domain = "denkmal.dev"
 
+  include 'cm::services'
+
   cm::application{'denkmal.org':
     path => '/app/denkmal.org',
     web => true,
+    debug => true,
     vhosts => {
       "www.${domain}" => {
         aliases => ["denkmal.dev", "admin.${domain}"],

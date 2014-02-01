17cfe2b18ea0703bc5ac6f9deeb516d52fd4b491
Reto Kaiser <reto@cargomedia.ch>
2014-02-01 18:05:03 +0100

    Formatting

--- scripts/cm.php
+++ scripts/cm.php
@@ -4,7 +4,7 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 $bootloader = new Denkmal_Bootloader(dirname(__DIR__) . '/', 'library/');
 $bootloader->setEnvironment('cli');
-$bootloader->load(array('errorHandler', 'constants', 'exceptionHandler','defaults'));
+$bootloader->load(array('errorHandler', 'constants', 'exceptionHandler', 'defaults'));
 
 $manager = new CM_Cli_CommandManager();
 $manager->autoloadCommands();

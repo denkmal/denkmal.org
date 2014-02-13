26329e177faa663ec3c0d7708facbf9ef2555e6c
tomaszdurka <tomasz@durka.pl>
2014-02-13 13:24:39 +0100

    Fix create usage in fillExampleData

--- library/Denkmal/library/Denkmal/App/Cli.php
+++ library/Denkmal/library/Denkmal/App/Cli.php
@@ -3,7 +3,7 @@
 class Denkmal_App_Cli extends CM_App_Cli {
 
 	public function fillExampleData() {
-		$admin = Denkmal_Model_User::create(array('email' => 'admin@denkmal.org', 'password' => 'admin'));
+		$admin = Denkmal_Model_User::create('admin@denkmal.org', 'admin');
 		$admin->getRoles()->add(Denkmal_Role::ADMIN);
 	}
 

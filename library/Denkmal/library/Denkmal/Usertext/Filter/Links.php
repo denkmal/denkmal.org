38c1b22fca317c69a6536005eb18673f6211087b
Reto Kaiser <reto@cargomedia.ch>
2014-04-12 19:21:05 +0200

    Cache eventtext replacements in local cache

--- library/Denkmal/library/Denkmal/Usertext/Filter/Links.php
+++ library/Denkmal/library/Denkmal/Usertext/Filter/Links.php
@@ -4,20 +4,56 @@ class Denkmal_Usertext_Filter_Links implements CM_Usertext_Filter_Interface {
 
     public function transform($text, CM_Render $render) {
         $text = (string) $text;
-        $wordBoundary = '([^\w]|^|$)';
-        /** @var $link Denkmal_Model_Link */
-        foreach (new Denkmal_Paging_Link_All() as $link) {
-            if (false === stripos($text, $link->getLabel())) {
+        foreach ($this->_getReplacements() as $replacement) {
+            if (false === stripos($text, $replacement['label'])) {
                 continue;
             }
-            if (!$link->getAutomatic()) {
-                $search = '#' . $wordBoundary . '\[' . preg_quote($link->getLabel()) . '\]' . $wordBoundary . '#ui';
-            } else {
-                $search = '#' . $wordBoundary . preg_quote($link->getLabel()) . $wordBoundary . '#ui';
-            }
-            $replace = '$1<a href="' . $link->getUrl() . '" class="url" target="_blank">' . $link->getLabel() . '</a>$2';
-            $text = preg_replace($search, $replace, $text);
+            $text = preg_replace($replacement['search'], $replacement['replace'], $text);
         }
         return $text;
+    }
+
+    /**
+     * @return array
+     */
+    private function _getReplacements() {
+        $cacheKey = self::_getCacheKey();
+        $cache = CM_Cache_Local::getInstance();
+        if (($replacements = $cache->get($cacheKey)) === false) {
+            $wordBoundary = '([^\w]|^|$)';
+            $replacements = array();
+            $linkList = new Denkmal_Paging_Link_All('label,url,automatic');
+            foreach ($linkList->getItemsRaw() as $linkRow) {
+                $label = (string) $linkRow['label'];
+                $url = (string) $linkRow['url'];
+                $automatic = (bool) $linkRow['automatic'];
+                if (!$automatic) {
+                    $search = '#' . $wordBoundary . '\[' . preg_quote($label) . '\]' . $wordBoundary . '#ui';
+                } else {
+                    $search = '#' . $wordBoundary . preg_quote($label) . $wordBoundary . '#ui';
+                }
+                $replace = '$1<a href="' . $url . '" class="url" target="_blank">' . $label . '</a>$2';
+                $replacements[] = array(
+                    'label'   => $label,
+                    'search'  => $search,
+                    'replace' => $replace,
+                );
+            }
+            $cache->set($cacheKey, $replacements);
+        }
+        return $replacements;
+    }
+
+    public static function clearCache() {
+        $cacheKey = self::_getCacheKey();
+        $cache = CM_Cache_Local::getInstance();
+        $cache->delete($cacheKey);
+    }
+
+    /**
+     * @return string
+     */
+    private static function _getCacheKey() {
+        return 'Denkmal_Usertext_Filter_Links';
     }
 }

733d2d03429624b881fa810629a7fab2e53f4797
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-04-13 15:35:42 +0200

    fixed paging in admin lists

--- library/Admin/library/Admin/Component/EventList/Abstract.php
+++ library/Admin/library/Admin/Component/EventList/Abstract.php
@@ -3,7 +3,7 @@
 class Admin_Component_EventList_Abstract extends Admin_Component_Abstract {
 
     protected function _prepareList(CM_Paging_Abstract $eventList) {
-        $eventList->setPage($this->_params->getPage(), $this->_params->getInt('count', 50));
+        $eventList->setPage($this->_params->getPage(), $this->_params->getInt('count', 10));
 
         $this->setTplParam('eventList', $eventList);
     }

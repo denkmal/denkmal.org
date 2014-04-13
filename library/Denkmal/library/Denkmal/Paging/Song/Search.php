24ad23f828ab5e9447a74c470877e79d4c0abda7
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-04-13 17:57:12 +0200

    allow parsing array of terms to song search paging

--- library/Denkmal/library/Denkmal/Paging/Song/Search.php
+++ library/Denkmal/library/Denkmal/Paging/Song/Search.php
@@ -3,11 +3,19 @@
 class Denkmal_Paging_Song_Search extends Denkmal_Paging_Song_Abstract {
 
     /**
-     * @param string $term
+     * @param string|string[] $term
      */
     public function __construct($term) {
-        $term = (string) $term;
-        $source = new CM_PagingSource_Sql('id', 'denkmal_model_song', 'LOWER(`label`) LIKE ?', '`label`', null, null, array('%' . strtolower($term) . '%'));
+        $term = (array) $term;
+
+        $whereList = array_fill(0, count($term), 'LOWER(`label`) LIKE ?');
+        $where = implode(' AND ', $whereList);
+
+        $params = Functional\map($term, function ($value) {
+            return '%' . strtolower($value) . '%';
+        });
+
+        $source = new CM_PagingSource_Sql('id', 'denkmal_model_song', $where, '`label`', null, null, $params);
         parent::__construct($source);
     }
 }

bf71ab67994801950e23eed79d9deca0408af0ab
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-04-13 21:07:58 +0200

    rename var and user currentTarget

--- library/Admin/library/Admin/Component/Event.js
+++ library/Admin/library/Admin/Component/Event.js
@@ -9,8 +9,8 @@ var Admin_Component_Event = Admin_Component_Abstract.extend({
 
   events: {
     'click .selectSong': function(event) {
-      var song = $(event.target).closest('.selectSong');
-      this.findChild('Admin_Form_Event').getField('song').setValue({'id': song.attr('data-id'), 'name': song.attr('data-label')});
+      var $song = $(event.currentTarget).closest('.selectSong');
+      this.findChild('Admin_Form_Event').getField('song').setValue({'id': $song.attr('data-id'), 'name': $song.attr('data-label')});
     }
   }
 });

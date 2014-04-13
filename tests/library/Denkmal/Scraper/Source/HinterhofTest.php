a9441f1a8b50e6eafa709d643f4ca80bd1a4f7c0
Reto Kaiser <reto@cargomedia.ch>
2014-04-13 22:09:06 +0200

    Add hinterhof scraper

---
+++ tests/library/Denkmal/Scraper/Source/HinterhofTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class Denkmal_Scraper_Source_HinterhofTest extends CMTest_TestCase {
+
+    public function testProcessPageDate() {
+        $html = Denkmal_Scraper_Source_Abstract::loadFile(DIR_TEST_DATA . 'hinterhof.html');
+
+        $scraper = $this->getMockBuilder('Denkmal_Scraper_Source_Hinterhof')->setMethods(array('_addEventAndVenue'))->getMock();
+        $scraper->expects($this->exactly(27))->method('_addEventAndVenue');
+        $scraper->expects($this->at(0))->method('_addEventAndVenue')->with(
+            'Hinterhof',
+            new Denkmal_Scraper_Description('Hinterhof Slam', null, new Denkmal_Scraper_Genres('slam poetry, whiskey grooves')),
+            new DateTime('2014-04-17 20:00:00')
+        );
+        $scraper->expects($this->at(1))->method('_addEventAndVenue')->with(
+            'Hinterhof',
+            new Denkmal_Scraper_Description('The Correspondents | Live', 'Bandura & Hinterhof “Bitten zum” Tanz', new Denkmal_Scraper_Genres('funk beats, swing hop, world grooves')),
+            new DateTime('2014-04-17 20:00:00')
+        );
+        $scraper->expects($this->at(16))->method('_addEventAndVenue')->with(
+            'Hinterhof',
+            new Denkmal_Scraper_Description('Dachterasse geöffnet'),
+            new DateTime('2014-05-07 20:00:00')
+        );
+
+        /** @var Denkmal_Scraper_Source_Hinterhof $scraper */
+        $scraper->processPage($html);
+    }
+}

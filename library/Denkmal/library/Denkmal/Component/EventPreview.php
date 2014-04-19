cab15abf2b5b0e69448d70bbf75fd849c978786c
Reto Kaiser <reto@cargomedia.ch>
2014-04-19 17:19:22 +0200

    Add info about duplicate events in event-preview

--- library/Denkmal/library/Denkmal/Component/EventPreview.php
+++ library/Denkmal/library/Denkmal/Component/EventPreview.php
@@ -7,13 +7,18 @@ class Denkmal_Component_EventPreview extends Denkmal_Component_Abstract {
         $venue = $this->_params->has('venue') ? $this->_params->getVenue('venue') : $event->getVenue();
 
         $fromDate = $event->getFrom();
-
         $fromDateDisplay = clone $fromDate;
         $fromDateDisplay->sub(new DateInterval('PT' . Denkmal_Site::getDayOffset() . 'H'));
 
         if ($fromDate->format('d.m.Y') != $fromDateDisplay->format('d.m.Y')) {
             $this->setTplParam('fromDateDisplay', $fromDateDisplay);
         }
+
+        if ($venue->hasIdRaw()) {
+            $eventDuplicates = new Admin_Paging_Event_VenueDate($event->getFrom(), $venue);
+            $this->setTplParam('eventDuplicates', $eventDuplicates);
+        }
+
         $this->setTplParam('event', $event);
         $this->setTplParam('venue', $venue);
 

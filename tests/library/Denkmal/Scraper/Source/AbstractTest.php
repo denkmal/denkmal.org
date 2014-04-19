cdefa78fba2abc096e7344254c0ba3db57de9004
Reto Kaiser <reto@cargomedia.ch>
2014-04-19 12:41:57 +0200

    Fix events-per-venueDate paging to reset time to 0:00

--- tests/library/Denkmal/Scraper/Source/AbstractTest.php
+++ tests/library/Denkmal/Scraper/Source/AbstractTest.php
@@ -48,6 +48,24 @@ class Denkmal_Scraper_Source_AbstractTest extends CMTest_TestCase {
         $this->assertSame(false, $method->invoke($scraper, $venue, $description, $from, $until));
     }
 
+    public function testIsValidEventExistingEventBeforeNewEvent() {
+        $venue = Denkmal_Model_Venue::create('foo', false, false);
+        $description = new Denkmal_Scraper_Description('foo');
+
+        $date = $this->_getDate();
+        $date->setTime(23, 0);
+
+        $dateExisting = clone $date;
+        $dateExisting->setTime(22, 0);
+        $eventExisting = Denkmal_Model_Event::create($venue, 'bar', false, false, $dateExisting);
+
+        $scraper = $this->getMockBuilder('Denkmal_Scraper_Source_Abstract')->getMockForAbstractClass();
+        /** @var Denkmal_Scraper_Source_Abstract $scraper */
+
+        $method = CMTest_TH::getProtectedMethod('Denkmal_Scraper_Source_Abstract', '_isValidEvent');
+        $this->assertSame(false, $method->invoke($scraper, $venue, $description, $date));
+    }
+
     /**
      * @return DateTime
      */

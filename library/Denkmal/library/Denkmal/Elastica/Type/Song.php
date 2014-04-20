e743e50c1171169645358db41ade3e1d8a9217fe
Reto Kaiser <reto@cargomedia.ch>
2014-04-20 15:01:08 +0200

    Add elastica index and searchQuery for song

---
+++ library/Denkmal/library/Denkmal/Elastica/Type/Song.php
@@ -0,0 +1,49 @@
+<?php
+
+class Denkmal_Elastica_Type_Song extends CM_Elastica_Type_Abstract {
+
+    const INDEX_NAME = 'song';
+
+    protected $_mapping = array(
+        'label' => array('type' => 'string'),
+    );
+
+    protected $_indexParams = array(
+        'index' => array(
+            'number_of_shards'   => 1,
+            'number_of_replicas' => 0
+        ),
+    );
+
+    protected function _getQuery($ids = null, $limit = null) {
+        $query = '
+            SELECT song.id, song.label
+			FROM `denkmal_model_song` song
+	    ';
+
+        if (is_array($ids)) {
+            $query .= ' WHERE song.id IN (' . implode(',', $ids) . ')';
+        }
+        if (($limit = (int) $limit) > 0) {
+            $query .= ' LIMIT ' . $limit;
+        }
+        return $query;
+    }
+
+    protected function _getDocument(array $data) {
+        $doc = new Elastica_Document($data['id'],
+            array(
+                'label' => $data['label'],
+            )
+        );
+        return $doc;
+    }
+
+    /**
+     * @param Denkmal_Model_Song $item
+     * @return int
+     */
+    protected static function _getIdForItem($item) {
+        return $item->getId();
+    }
+}
---
+++ tests/library/Denkmal/Elastica/Type/SongTest.php
@@ -0,0 +1,61 @@
+<?php
+
+class Denkmal_Elastica_Type_SongTest extends CMTest_TestCase {
+
+    /** @var Denkmal_Elastica_Type_Song */
+    protected static $_type;
+
+    /** @var CM_Search_Index_Cli */
+    protected static $_searchIndexCli;
+
+    public function setUp() {
+        CM_Config::get()->CM_Search->enabled = true;
+
+        self::$_type = new Denkmal_Elastica_Type_Song();
+        self::$_searchIndexCli = new CM_Search_Index_Cli();
+        self::$_searchIndexCli->create(self::$_type->getIndex()->getName());
+    }
+
+    public function tearDown() {
+        self::$_type->getIndex()->delete();
+        CMTest_TH::clearEnv();
+    }
+
+    public function testCrud() {
+        $file = new CM_File(DIR_TEST_DATA . 'music.mp3');
+        $song1 = Denkmal_Model_Song::create('foo', $file);
+        $song2 = Denkmal_Model_Song::create('bar', $file);
+        $song3 = Denkmal_Model_Song::create('bar foo', $file);
+        self::$_searchIndexCli->update(self::$_type->getIndex()->getName());
+
+        $searchQuery = new Denkmal_SearchQuery_Song();
+        $searchQuery->queryText('foo');
+        $searchQuery->sortLabel();
+
+        $pagingSource = new CM_PagingSource_Search(self::$_type, $searchQuery);
+        $this->assertEquals(array($song3->getId(), $song1->getId()), $pagingSource->getItems());
+
+        $song2->setLabel('aaa foo');
+        self::$_searchIndexCli->update(self::$_type->getIndex()->getName());
+        $this->assertEquals(array($song2->getId(), $song3->getId(), $song1->getId()), $pagingSource->getItems());
+
+        $song3->delete();
+        self::$_searchIndexCli->update(self::$_type->getIndex()->getName());
+        print_r($pagingSource->getItems());
+        $this->assertEquals(array($song2->getId(), $song1->getId()), $pagingSource->getItems());
+    }
+
+    public function testQueryText() {
+        $file = new CM_File(DIR_TEST_DATA . 'music.mp3');
+        $song1 = Denkmal_Model_Song::create('foo bar', $file);
+        $song2 = Denkmal_Model_Song::create('bar', $file);
+        self::$_searchIndexCli->update(self::$_type->getIndex()->getName());
+
+        $searchQuery = new Denkmal_SearchQuery_Song();
+        $searchQuery->queryText('Foo');
+
+        $pagingSource = new CM_PagingSource_Search(self::$_type, $searchQuery);
+        $this->assertEquals(array($song1->getId()), $pagingSource->getItems());
+    }
+
+}

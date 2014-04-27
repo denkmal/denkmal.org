76ec386d99bce9c62b0b0a57167d556339b5c704
Reto Kaiser <reto@cargomedia.ch>
2014-04-27 15:41:16 +0200

    Change scraper order

--- library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
+++ library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
@@ -5,8 +5,8 @@ class Denkmal_Paging_ScraperSource_All extends CM_Paging_Abstract {
     public function __construct() {
         $scraperList = array(
             new Denkmal_Scraper_Source_Programmzeitung(),
-            new Denkmal_Scraper_Source_Hinterhof(),
             new Denkmal_Scraper_Source_Fingerzeig(),
+            new Denkmal_Scraper_Source_Hinterhof(),
         );
 
         $source = new CM_PagingSource_Array($scraperList);

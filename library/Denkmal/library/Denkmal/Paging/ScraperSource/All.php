442dccef79c8d18bd9d7a3c25c17d53097c04398
Reto Kaiser <reto@cargomedia.ch>
2014-04-27 17:30:23 +0200

    Add lastfm scraper

--- library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
+++ library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
@@ -5,8 +5,9 @@ class Denkmal_Paging_ScraperSource_All extends CM_Paging_Abstract {
     public function __construct() {
         $scraperList = array(
             new Denkmal_Scraper_Source_Programmzeitung(),
-            new Denkmal_Scraper_Source_Fingerzeig(),
             new Denkmal_Scraper_Source_Hinterhof(),
+            new Denkmal_Scraper_Source_Fingerzeig(),
+            new Denkmal_Scraper_Source_Lastfm(),
         );
 
         $source = new CM_PagingSource_Array($scraperList);

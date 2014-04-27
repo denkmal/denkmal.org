5c00c41b8d1776adcfc680206ffcb4895505e064
Reto Kaiser <reto@cargomedia.ch>
2014-04-27 15:40:35 +0200

    Add fingerzeig to scraper list

--- library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
+++ library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
@@ -6,6 +6,7 @@ class Denkmal_Paging_ScraperSource_All extends CM_Paging_Abstract {
         $scraperList = array(
             new Denkmal_Scraper_Source_Programmzeitung(),
             new Denkmal_Scraper_Source_Hinterhof(),
+            new Denkmal_Scraper_Source_Fingerzeig(),
         );
 
         $source = new CM_PagingSource_Array($scraperList);

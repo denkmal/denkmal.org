35613569098458ae28c97a72b79678856db2cdc1
Reto Kaiser <reto@cargomedia.ch>
2014-05-11 16:43:00 +0200

    Add suspended page and layout

--- library/Denkmal/library/Denkmal/Page/Suspended.php
+++ library/Denkmal/library/Denkmal/Page/Suspended.php
@@ -8,4 +8,8 @@ class Denkmal_Page_Suspended extends Denkmal_Page_Abstract {
 
         $this->setTplParam('suspension', $suspension);
     }
+
+    public function getLayout(CM_Site_Abstract $site, $layoutName = null) {
+        return new Denkmal_Layout_Suspended($this);
+    }
 }

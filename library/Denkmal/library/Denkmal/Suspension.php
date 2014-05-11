daf8abc60f3e408b55830a184606834aac211ca6
Reto Kaiser <reto@cargomedia.ch>
2014-05-11 15:57:50 +0200

    Rename suspension option key

--- library/Denkmal/library/Denkmal/Suspension.php
+++ library/Denkmal/library/Denkmal/Suspension.php
@@ -6,14 +6,14 @@ class Denkmal_Suspension extends CM_Class_Abstract {
      * @return DateTime|null
      */
     public function getUntil() {
-        return CM_Option::getInstance()->get('suspendedUntil');
+        return CM_Option::getInstance()->get('denkmal.suspension.until');
     }
 
     /**
      * @param DateTime|null $until
      */
     public function setUntil(DateTime $until = null) {
-        CM_Option::getInstance()->set('suspendedUntil', $until);
+        CM_Option::getInstance()->set('denkmal.suspension.until', $until);
     }
 
     /**

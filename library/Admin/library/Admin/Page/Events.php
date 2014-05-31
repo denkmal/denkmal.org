5737391bff0f958effb9adef1a8e1823989bcbd7
Reto Kaiser <reto@cargomedia.ch>
2014-05-31 20:33:40 +0200

    Adjust Page::prepareResponse

--- library/Admin/library/Admin/Page/Events.php
+++ library/Admin/library/Admin/Page/Events.php
@@ -2,7 +2,7 @@
 
 class Admin_Page_Events extends Admin_Page_Abstract {
 
-    public function prepareResponse(CM_Response_Page $response) {
+    public function prepareResponse(CM_Frontend_Environment $environment, CM_Response_Page $response) {
         if (!$this->_params->has('date') && !$this->_params->has('searchTerm')) {
             $now = Denkmal_Site::getCurrentDate();
             $response->redirect('Admin_Page_Events', array('date' => $now->format('Y-n-j')));
--- library/Denkmal/library/Denkmal/Page/Error/NotFound.php
+++ library/Denkmal/library/Denkmal/Page/Error/NotFound.php
@@ -2,7 +2,7 @@
 
 class Denkmal_Page_Error_NotFound extends Denkmal_Page_Abstract {
 
-    public function prepareResponse(CM_Response_Page $response) {
+    public function prepareResponse(CM_Frontend_Environment $environment, CM_Response_Page $response) {
         $response->setHeaderNotfound();
     }
 }

5ba003d5cd08451c0be94e59be2dc951f722b830
Reto Kaiser <reto@cargomedia.ch>
2014-05-31 18:41:13 +0200

    Adjust checkAccessible

--- library/Admin/library/Admin/Component/LogList.php
+++ library/Admin/library/Admin/Component/LogList.php
@@ -2,14 +2,14 @@
 
 class Admin_Component_LogList extends CM_Component_LogList {
 
-    public function checkAccessible(CM_Render $render) {
-        if (!$this->_getViewer(true)->getRoles()->contains(Denkmal_Role::ADMIN)) {
+    public function checkAccessible(CM_Frontend_Environment $environment) {
+        if (!$environment->getViewer(true)->getRoles()->contains(Denkmal_Role::ADMIN)) {
             throw new CM_Exception_AuthRequired();
         }
     }
 
-    protected static function _getAllowedFlush(CM_Render $render) {
-        $viewer = $render->getViewer();
+    protected function _getAllowedFlush(CM_Frontend_Environment $environment) {
+        $viewer = $environment->getViewer();
         return $viewer && $viewer->getRoles()->contains(Denkmal_Role::ADMIN);
     }
 }
--- library/Denkmal/library/Denkmal/Component/Abstract.php
+++ library/Denkmal/library/Denkmal/Component/Abstract.php
@@ -5,6 +5,6 @@ abstract class Denkmal_Component_Abstract extends CM_Component_Abstract {
     /** @var  Denkmal_Params */
     protected $_params;
 
-    public function checkAccessible(CM_Render $render) {
+    public function checkAccessible(CM_Frontend_Environment $environment) {
     }
 }

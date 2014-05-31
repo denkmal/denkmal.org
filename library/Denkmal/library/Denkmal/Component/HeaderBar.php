554d37b177bdf541319480a276d2b4db7a96de79
Reto Kaiser <reto@cargomedia.ch>
2014-05-31 20:25:10 +0200

    Implement prepare() in cmps

--- library/Denkmal/library/Denkmal/Component/HeaderBar.php
+++ library/Denkmal/library/Denkmal/Component/HeaderBar.php
@@ -2,4 +2,6 @@
 
 class Denkmal_Component_HeaderBar extends Denkmal_Component_Abstract {
 
+    public function prepare(CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
+    }
 }

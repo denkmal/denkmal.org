fc8d63021a23d375c83e09c8517ef565a55526b2
Reto Kaiser <reto@cargomedia.ch>
2014-10-07 21:05:10 +0200

    Remove debug code

--- tests/library/Denkmal/Scraper/Source/KaschemmeTest.php
+++ tests/library/Denkmal/Scraper/Source/KaschemmeTest.php
@@ -27,30 +27,3 @@ class Denkmal_Scraper_Source_KaschemmeTest extends CMTest_TestCase {
         $scraper->processPage($html);
     }
 }
-
-
-
-/**
- * <div class="project_content">
-Mittwoch 01.10.2014 /// FC Basel 1893 - Liverpool FC /// 18h<br />
-......................................................................................................<br />
-Freitag 03.10.2014 /// Kaschemme SKANK w/ Echolt /// 22h<br />
-......................................................................................................<br />
-Samstag 04.10.2014 /// Gestern Nacht in der Kaschemme w/ Herzschwester & Thom Nagy 22h<br />
-......................................................................................................<br />
-Sonntag 05.10.2014 /// Ebo Taylor LIVE! w/ Konzeptlos /// 18h<br />
-......................................................................................................<br />
-Freitag 10.10.2014 /// WeMakeIt "Exclusiv Party" w/ Dj Mixwell, Konzeptlos & Goldfinger Brothers (Nur mit Einladung oder Wemakeit Sponsoring!!!) 20h<br />
-......................................................................................................<br />
-Samstag 11.10.2014 /// Kaschemme Cypher Hostet by Kalmoo w/ Levo Rimed (K.W.A.T.) Dj's: Giddla (TNN) & DJ ALK aka Dimes /// 22h<br />
-......................................................................................................<br />
-Freitag 17.10.2014 /// Xplcit Contents After Party /// 20h<br />
-Samstag 18.10.2014 ///Love Tempo w/ Kejeblos (Phantom Island Records, Zürich), Hotmail, Akay, Neevo /// 22h<br />
-......................................................................................................<br />
-Freitag 24.10.2014 /// ça Claque Label Night w/ Nunu (Strasbourg, FR), Lord Soft, B.O.M., Larry King, Goldfinger Brothers 22h<br />
-......................................................................................................<br />
-Samstag 25.10.2014 /// Beatklinik pres. Calibro 35 (IT) LIVE /// 22h<br />
-......................................................................................................<br />
-Freitag 31.10.2014 /// Akiwawa w/ Dj Format (UK), Pun & Rainer /// 22h<br />
-
- */

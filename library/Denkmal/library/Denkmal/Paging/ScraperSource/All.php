a6176aa4ce45cf171c01e3223b4c28f6a71dd754
Reto Kaiser <reto@cargomedia.ch>
2014-10-08 22:08:26 +0200

    Move kaschemme scraper up

--- library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
+++ library/Denkmal/library/Denkmal/Paging/ScraperSource/All.php
@@ -4,11 +4,11 @@ class Denkmal_Paging_ScraperSource_All extends CM_Paging_Abstract {
 
     public function __construct() {
         $scraperList = array(
+            new Denkmal_Scraper_Source_Kaschemme(),
             new Denkmal_Scraper_Source_Programmzeitung(),
             new Denkmal_Scraper_Source_Hinterhof(),
             new Denkmal_Scraper_Source_Fingerzeig(),
             new Denkmal_Scraper_Source_Lastfm(),
-            new Denkmal_Scraper_Source_Kaschemme(),
         );
 
         $source = new CM_PagingSource_Array($scraperList);

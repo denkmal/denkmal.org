daf355cb8f7f22dd315c4d426484efe614a8f386
Reto Kaiser <reto@cargomedia.ch>
2014-10-12 17:57:10 +0200

    Add paging for venues by coordinates

---
+++ library/Denkmal/library/Denkmal/Paging/Venue/GeoPoint.php
@@ -0,0 +1,28 @@
+<?php
+
+class Denkmal_Paging_Venue_GeoPoint extends Denkmal_Paging_Venue_Abstract {
+
+    /**
+     * @param CM_Geo_Point $point
+     * @param int          $distanceMax
+     */
+    public function __construct(CM_Geo_Point $point, $distanceMax) {
+        $lat = $point->getLatitude();
+        $lon = $point->getLongitude();
+        $earthRadius = 6371009;
+
+        $pi180 = M_PI / 180;
+        $metersPerDegreeEquator = $earthRadius * $pi180;
+        $metersPerDegree = $metersPerDegreeEquator * cos($lat * $pi180);
+
+        $latMin = $lat - $distanceMax / $metersPerDegreeEquator;
+        $latMax = $lat + $distanceMax / $metersPerDegreeEquator;
+        $lonMin = $lon - $distanceMax / $metersPerDegree;
+        $lonMax = $lon + $distanceMax / $metersPerDegree;
+
+        $where = "MBRContains(LineString(Point($latMax, $lonMax), Point($latMin, $lonMin)), Point(`latitude`, `longitude`))";
+        $order = "((POW($lat - `latitude`, 2)) + (POW($lon - `longitude`, 2))) ASC";
+        $source = new CM_PagingSource_Sql('id', 'denkmal_model_venue', $where, $order);
+        parent::__construct($source);
+    }
+}

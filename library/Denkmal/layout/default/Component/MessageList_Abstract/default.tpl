c897ede17888d75f834472b3f8490d2bc4e13836
Reto Kaiser <reto@cargomedia.ch>
2014-10-12 20:27:37 +0200

    Render and add message on client

--- library/Denkmal/layout/default/Component/MessageList_Abstract/default.tpl
+++ library/Denkmal/layout/default/Component/MessageList_Abstract/default.tpl
@@ -22,3 +22,26 @@
     </li>
   {/foreach}
 </ul>
+
+<script type="text/template" class="template-message">
+  <li class="message" data-id="[[-id]]">
+    [[print(cm.date.timeago(created))]]
+
+    <div class="message-location nowrap">
+      [[-venue]]
+    </div>
+
+    <div class="message-content">
+      [[ if (hasText) { ]]
+        <div class="message-text">
+          [[-text]]
+        </div>
+      [[ } ]]
+      [[ if (hasImage) { ]]
+        <div class="message-image">
+          <img src="[[-imageUrl]]" />
+        </div>
+      [[ } ]]
+    </div>
+  </li>
+</script>
--- library/Denkmal/library/Denkmal/Component/MessageList/All.js
+++ library/Denkmal/library/Denkmal/Component/MessageList/All.js
@@ -9,7 +9,22 @@ var Denkmal_Component_MessageList_All = Denkmal_Component_MessageList_Abstract.e
 
   ready: function() {
     this.bindStream('global-internal', cm.model.types.CM_Model_StreamChannel_Message, 'message-create', function(message) {
-      console.log('message', message);
+      this._addMessage(message);
     });
+  },
+
+  /**
+   * @param {Object} message
+   */
+  _addMessage: function(message) {
+    this.renderTemplate('template-message', {
+      id: message.id,
+      created: message.created,
+      venue: message.venue.name,
+      hasText: message.text !== null,
+      text: message.text,
+      hasImage: message.image !== null,
+      imageUrl: (message.image !== null) ? message.image['url-thumb'] : null
+    }).appendTo(this.$('.messageList'));
   }
 });

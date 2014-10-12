051af89ef8671e5785078a052a78d382e161d58d
Reto Kaiser <reto@cargomedia.ch>
2014-10-12 16:47:31 +0200

    Add "chat" page

---
+++ library/Denkmal/library/Denkmal/Page/Chat.js
@@ -0,0 +1,9 @@
+/**
+ * @class Denkmal_Page_Chat
+ * @extends Denkmal_Page_Abstract
+ */
+var Denkmal_Page_Chat = Denkmal_Page_Abstract.extend({
+
+  /** @type String */
+  _class: 'Denkmal_Page_Chat'
+});
---
+++ library/Denkmal/library/Denkmal/Page/Chat.php
@@ -0,0 +1,5 @@
+<?php
+
+class Denkmal_Page_Chat extends Denkmal_Page_Abstract {
+
+}

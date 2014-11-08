f3f407ba1f9d2377ddf2c0c12a3394f46839f568
Reto Kaiser <reto@cargomedia.ch>
2014-11-08 18:45:40 +0100

    Adjust kaschemme scraper

--- library/Denkmal/library/Denkmal/Scraper/Source/Kaschemme.php
+++ library/Denkmal/library/Denkmal/Scraper/Source/Kaschemme.php
@@ -16,22 +16,54 @@ class Denkmal_Scraper_Source_Kaschemme extends Denkmal_Scraper_Source_Abstract {
         $html = new CM_Dom_NodeList($html, true);
         $venueName = 'Kaschemme';
         $eventHtml = $html->find('#maincontainer .project_content')->getHtml();
-        $regexp = '(?<weekday>\w+)\s+(?<day>\d+)\.(?<month>\d+)\.(?<year>\d+)\s+(?<description>.+?)\s+(?<hour>\d+)h\s*<br';
+        $regexp = '(?<weekday>\w+)\s+(?<day>\d+)\.(?<month>\d+)\.(?<year>\d+)\s+(?<title>.+?)\s*<br>(?<description>.+?)?\.{4,}';
         preg_match_all('#' . $regexp . '#u', $eventHtml, $matches, PREG_SET_ORDER);
 
         foreach ($matches as $match) {
             $from = new Denkmal_Scraper_Date($match['day'], $match['month'], $match['year']);
-            $from->setTime($match['hour']);
-            $description = $match['description'];
-            $description = html_entity_decode($description);
-            $description = preg_replace('#^///\s*#', '', $description);
-            $description = preg_replace('#\s*///$#', '', $description);
+            $from->setTime(22);
+
+            $title = $this->_parseText($match['title']);
+            $descriptionList = explode('<br>', $match['description']);
+            $descriptionList = Functional\map($descriptionList, function ($descriptionItem) {
+                return $this->_parseText($descriptionItem);
+            });
+            $descriptionList = array_filter($descriptionList);
+
+            $genres = null;
+            foreach ($descriptionList as $i => $descriptionItem) {
+                if (preg_match('#^Sounds Like\s*: \(?(?<genres>.+?)\)?$#ui', $descriptionItem, $matchesGenres)) {
+                    $genres = new Denkmal_Scraper_Genres($matchesGenres['genres']);
+                    unset($descriptionList[$i]);
+                }
+            }
+
+            $description = implode(', ', $descriptionList);
+
+            $title = $this->_parseText($title);
+            $description = $this->_parseText($description);
+            if (empty($description)) {
+                $description = $title;
+                $title = null;
+            }
 
             $this->_addEventAndVenue(
                 $venueName,
-                new Denkmal_Scraper_Description($description, null),
+                new Denkmal_Scraper_Description($description, $title, $genres),
                 $from->getDateTime()
             );
         }
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    private function _parseText($text) {
+        $text = html_entity_decode($text);
+        $text = preg_replace('#^///\s*#', '', $text);
+        $text = preg_replace('#\s*///$#', '', $text);
+        $text = trim($text);
+        return $text;
     }
 }

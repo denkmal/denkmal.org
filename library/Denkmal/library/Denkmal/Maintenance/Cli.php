571e9c5a691c70060947a2f3da4789e11f90d857
Reto Kaiser <reto@cargomedia.ch>
2014-11-22 00:05:00 +0100

    Formatting

--- library/Denkmal/library/Denkmal/Maintenance/Cli.php
+++ library/Denkmal/library/Denkmal/Maintenance/Cli.php
@@ -7,6 +7,7 @@ class Denkmal_Maintenance_Cli extends CM_Maintenance_Cli {
      */
     protected function _registerCallbacks() {
         parent::_registerCallbacks();
+
         $this->_registerClockworkCallbacks('12 hours', array(
             'Check links' => function () {
                     $linkList = new Denkmal_Paging_Link_All();

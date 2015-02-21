6e475b46d47cf168a0689db0869f98ac155c379b
Reto Kaiser <reto@cargomedia.ch>
2015-02-21 22:32:53 +0100

    Default venue.suspended to 0

--- library/Denkmal/resources/db/update/9.php
+++ library/Denkmal/resources/db/update/9.php
@@ -1,5 +1,5 @@
 <?php
 
 if (!CM_Db_Db::existsColumn('denkmal_model_venue', 'suspended')) {
-    CM_Db_Db::exec("ALTER TABLE denkmal_model_venue ADD COLUMN `suspended` tinyint(4) unsigned NOT NULL DEFAULT '1'");
+    CM_Db_Db::exec("ALTER TABLE denkmal_model_venue ADD COLUMN `suspended` tinyint(4) unsigned NOT NULL DEFAULT '0'");
 }

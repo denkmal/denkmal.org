b79b1b2a5d3754c0088ceeafc6136f8b7b9c4b84
Reto Kaiser <reto@cargomedia.ch>
2015-02-21 22:01:04 +0100

    Configure site URLs in travis config

--- tests/travis/config.php
+++ tests/travis/config.php
@@ -7,4 +7,7 @@ return function (CM_Config_Node $config) {
     $config->CM_Memcache_Client->servers = array(
       array('host' => 'localhost', 'port' => 11211),
     );
+
+    $config->Denkmal_Site->url = 'http://www.denkmal.dev';
+    $config->Admin_Site->url = 'http://admin.denkmal.dev';
 };

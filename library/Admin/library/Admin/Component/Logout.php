cdfcd1492de91c910819551b20085f79337a8205
Reto Kaiser <reto@cargomedia.ch>
2015-02-22 10:40:41 +0100

    Allow auth-required and logout to all users

--- library/Admin/library/Admin/Component/Logout.php
+++ library/Admin/library/Admin/Component/Logout.php
@@ -2,4 +2,6 @@
 
 class Admin_Component_Logout extends Admin_Component_Abstract {
 
+    public function checkAccessible(CM_Frontend_Environment $environment) {
+    }
 }
--- library/Admin/library/Admin/FormAction/Logout/Process.php
+++ library/Admin/library/Admin/FormAction/Logout/Process.php
@@ -2,6 +2,9 @@
 
 class Admin_FormAction_Logout_Process extends Admin_FormAction_Abstract {
 
+    protected function _checkData(CM_Params $params, CM_Http_Response_View_Form $response, CM_Form_Abstract $form) {
+    }
+
     protected function _process(CM_Params $params, CM_Http_Response_View_Form $response, CM_Form_Abstract $form) {
         $response->getRequest()->getSession()->deleteUser();
         $response->getRequest()->getSession()->setLifetime();

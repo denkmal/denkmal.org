169df751f0d7a7a8fd4868c8179a566151fc9adf
Reto Kaiser <reto@cargomedia.ch>
2015-02-23 21:20:41 +0100

    Define max length for message text

--- library/Denkmal/library/Denkmal/Form/Message.php
+++ library/Denkmal/library/Denkmal/Form/Message.php
@@ -4,7 +4,7 @@ class Denkmal_Form_Message extends CM_Form_Abstract {
 
     protected function _initialize() {
         $this->registerField(new Denkmal_FormField_VenueNearby(['name' => 'venue']));
-        $this->registerField(new CM_FormField_Text(['name' => 'text']));
+        $this->registerField(new CM_FormField_Text(['name' => 'text', 'lengthMax' => 500]));
         $this->registerField(new CM_FormField_FileImage(['name' => 'image']));
         $this->registerField(new Denkmal_FormField_Tags(['name' => 'tags', 'cardinality' => 3]));
 

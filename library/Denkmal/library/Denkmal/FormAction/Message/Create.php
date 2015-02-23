9125b83c3c071ca5f481f892fddac414c55bef8a
Reto Kaiser <reto@cargomedia.ch>
2015-02-23 11:45:12 +0100

    Allow to add tags to message, add test

--- library/Denkmal/library/Denkmal/FormAction/Message/Create.php
+++ library/Denkmal/library/Denkmal/FormAction/Message/Create.php
@@ -3,13 +3,20 @@
 class Denkmal_FormAction_Message_Create extends CM_FormAction_Abstract {
 
     protected function _getRequiredFields() {
-        return array('venue', 'text');
+        return array('venue');
+    }
+
+    protected function _checkData(CM_Params $params, CM_Http_Response_View_Form $response, CM_Form_Abstract $form) {
+        if (0 === count($params->get('tags')) && !$params->has('text')) {
+            $response->addError($response->getRender()->getTranslation('Bitte Nachricht eingeben'));
+        }
     }
 
     protected function _process(CM_Params $params, CM_Http_Response_View_Form $response, CM_Form_Abstract $form) {
         /** @var Denkmal_Params $params */
         $venue = $params->getVenue('venue');
-        $text = $params->getString('text');
+        $text = $params->has('text') ? $params->getString('text') : null;
+        $viewer = $response->getViewer();
         $clientId = (string) $response->getRequest()->getClientId();
 
         $image = null;
@@ -19,9 +26,15 @@ class Denkmal_FormAction_Message_Create extends CM_FormAction_Abstract {
             $image = Denkmal_Model_MessageImage::create(new CM_File_Image($file));
         }
 
+        /** @var Denkmal_Model_Tag[] $tagList */
+        $tagList = $params->get('tags');
+
         $action = new Denkmal_Action_Message(Denkmal_Action_Message::CREATE, $response->getRequest()->getIp());
         $action->prepare();
-        $message = Denkmal_Model_Message::create($venue, $clientId, null, $text, $image);
+        $message = Denkmal_Model_Message::create($venue, $clientId, $viewer, $text, $image);
+        foreach($tagList as $tag) {
+            $message->getTags()->add($tag);
+        }
         $action->notify($message);
 
         return $message;
---
+++ tests/library/Denkmal/Form/MessageTest.php
@@ -0,0 +1,88 @@
+<?php
+
+class Denkmal_Form_MessageTest extends CMTest_TestCase {
+
+    protected function tearDown() {
+        CMTest_TH::clearEnv();
+    }
+
+    public function testProcessAll() {
+        $user = Denkmal_Model_User::create('foo@example.com', 'foo', 'pass');
+        $venue = Denkmal_Model_Venue::create('Foo', false, false);
+        $tag1 = Denkmal_Model_Tag::create('tag1');
+        $tag2 = Denkmal_Model_Tag::create('tag2');
+        $image = new CM_File(DIR_TEST_DATA . '/image.jpg');
+        $imageUsercontent = CM_File_UserContent_Temp::create('image.jpg', $image->read());
+
+        $form = new Denkmal_Form_Message();
+        $action = new Denkmal_FormAction_Message_Create($form);
+        $request = $this->createRequestFormAction($action, [
+            'venue' => $venue->getId(),
+            'text'  => 'Hello world',
+            'tags'  => CM_Params::jsonEncode([$tag1->getId(), $tag2->getId()]),
+            'image' => [$imageUsercontent->getUniqid()],
+        ]);
+        $request->mockMethod('getClientId')->set(12);
+        $request->mockMethod('getViewer')->set($user);
+        $response = new CM_Http_Response_View_Form($request, $this->getServiceManager());
+        $response->process();
+
+        $this->assertFormResponseSuccess($response);
+
+        /** @var Denkmal_Model_Message $message */
+        $message = (new Denkmal_Paging_Message_All())->getItem(0);
+
+        $this->assertEquals($venue, $message->getVenue());
+        $this->assertSame('12', $message->getClientId());
+        $this->assertEquals($user, $message->getUser());
+        $this->assertSame('Hello world', $message->getText());
+        $this->assertNotNull($message->getImage());
+        $this->assertEquals([$tag1, $tag2], $message->getTags()->getAll());
+    }
+
+    public function testProcessOnlyTags() {
+        $venue = Denkmal_Model_Venue::create('Foo', false, false);
+        $tag1 = Denkmal_Model_Tag::create('tag1');
+        $tag2 = Denkmal_Model_Tag::create('tag2');
+
+        $form = new Denkmal_Form_Message();
+        $action = new Denkmal_FormAction_Message_Create($form);
+        $request = $this->createRequestFormAction($action, [
+            'venue' => $venue->getId(),
+            'text'  => '',
+            'tags'  => CM_Params::jsonEncode([$tag1->getId(), $tag2->getId()]),
+        ]);
+        $request->mockMethod('getClientId')->set(12);
+        $response = new CM_Http_Response_View_Form($request, $this->getServiceManager());
+        $response->process();
+
+        $this->assertFormResponseSuccess($response);
+
+        /** @var Denkmal_Model_Message $message */
+        $message = (new Denkmal_Paging_Message_All())->getItem(0);
+
+        $this->assertEquals($venue, $message->getVenue());
+        $this->assertSame('12', $message->getClientId());
+        $this->assertEquals([$tag1, $tag2], $message->getTags()->getAll());
+        $this->assertSame(null, $message->getUser());
+        $this->assertSame(null, $message->getText());
+        $this->assertSame(null, $message->getImage());
+    }
+
+    public function testProcessNone() {
+        $venue = Denkmal_Model_Venue::create('Foo', false, false);
+
+        $form = new Denkmal_Form_Message();
+        $action = new Denkmal_FormAction_Message_Create($form);
+        $request = $this->createRequestFormAction($action, [
+            'venue' => $venue->getId(),
+            'text'  => '',
+            'tags'  => CM_Params::jsonEncode([]),
+        ]);
+        $request->mockMethod('getClientId')->set(12);
+        $response = new CM_Http_Response_View_Form($request, $this->getServiceManager());
+        $response->process();
+
+        $this->assertFormResponseError($response, 'Bitte Nachricht eingeben');
+    }
+}

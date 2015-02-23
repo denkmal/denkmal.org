373b5f76f80a268a9bf45efe86e8bf2aad9145fb
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 15:08:39 +0100

    Added ChangePassword form and action

---
+++ library/Denkmal/library/Denkmal/Form/ChangePassword.php
@@ -0,0 +1,12 @@
+<?php
+
+class Denkmal_Form_ChangePassword extends CM_Form_Abstract {
+
+    protected function _initialize() {
+        $this->registerField(new CM_FormField_Password(['name' => 'old_password']));
+        $this->registerField(new CM_FormField_Password(['name' => 'new_password']));
+        $this->registerField(new CM_FormField_Password(['name' => 'new_password_confirm']));
+
+        $this->registerAction(new Denkmal_FormAction_ChangePassword_Process($this));
+    }
+}

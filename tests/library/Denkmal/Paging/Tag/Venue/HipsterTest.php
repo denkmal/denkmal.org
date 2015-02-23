21382203afb2ca84f66e58d0c84f0eb191c89e42
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 22:44:45 +0100

    Improved tests

--- tests/library/Denkmal/Paging/Tag/Venue/HipsterTest.php
+++ tests/library/Denkmal/Paging/Tag/Venue/HipsterTest.php
@@ -23,9 +23,9 @@ class Denkmal_Paging_Tag_Venue_HipsterTest extends CMTest_TestCase {
     public function testAggregation() {
         $venue = Denkmal_Model_Venue::create('Example', true, false);
 
-        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, new DateTime());
-        $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, new DateTime());
-        $message3 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 3', null, new DateTime());
+        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, (new DateTime())->sub(new DateInterval('PT3S')));
+        $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, (new DateTime())->sub(new DateInterval('PT2S')));
+        $message3 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 3', null, (new DateTime())->sub(new DateInterval('PT1S')));
 
         $tag1 = Denkmal_Model_Tag::create('foo');
         $tag2 = Denkmal_Model_Tag::create('bar');
@@ -33,22 +33,20 @@ class Denkmal_Paging_Tag_Venue_HipsterTest extends CMTest_TestCase {
         $tag4 = Denkmal_Model_Tag::create('barfoo');
 
         $message1->getTags()->add($tag1);
-        $message1->getTags()->add($tag3);
 
         $message2->getTags()->add($tag2);
         $message2->getTags()->add($tag3);
 
         $message3->getTags()->add($tag4);
-        $message3->getTags()->add($tag1);
 
         $tagList = new Denkmal_Paging_Tag_Venue_Hipster($venue);
-        $this->assertEquals(array($tag1, $tag3, $tag2, $tag4), $tagList);
+        $this->assertEquals(array($tag4, $tag2, $tag3, $tag1), $tagList);
     }
 
     public function testTimeLimit() {
         $venue = Denkmal_Model_Venue::create('Example', true, false);
 
-        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, new DateTime('2014-02-22'));
+        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, (new DateTime('2014-02-22'))->sub(new DateInterval('PT2H')));
         $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, new DateTime());
 
         $tag1 = Denkmal_Model_Tag::create('foo');

b0fbcdf0b60814d50317fec65890445d72bc10be
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 15:09:52 +0100

    Added account page

---
+++ library/Denkmal/layout/default/Page/Account/default.tpl
@@ -0,0 +1,7 @@
+{extends file=$render->getLayoutPath('Page/Abstract/default.tpl')}
+
+{block name='content-title'}{/block}
+
+{block name='content-main'}
+  {component name='Denkmal_Component_Account'}
+{/block}
---
+++ library/Denkmal/library/Denkmal/Page/Account.js
@@ -0,0 +1,9 @@
+/**
+ * @class Denkmal_Page_Account
+ * @extends Denkmal_Page_Abstract
+ */
+var Denkmal_Page_Account = Denkmal_Page_Abstract.extend({
+
+	/** @type String */
+	_class: 'Denkmal_Page_Account'
+});

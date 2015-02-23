1be042d7ae870550b49005ea148f5cee9149cc53
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 22:59:00 +0100

    Added createdMin; moved JOIN;

--- library/Denkmal/library/Denkmal/Paging/Tag/Venue/Hipster.php
+++ library/Denkmal/library/Denkmal/Paging/Tag/Venue/Hipster.php
@@ -4,16 +4,21 @@ class Denkmal_Paging_Tag_Venue_Hipster extends Denkmal_Paging_Tag_Abstract {
 
     /**
      * @param Denkmal_Model_Venue $venue
+     * @param string|null         $createdMin
+     * @throws CM_Class_Exception_TypeNotConfiguredException
      */
-    public function __construct(Denkmal_Model_Venue $venue) {
+    public function __construct(Denkmal_Model_Venue $venue, $createdMin = null) {
+        if ($createdMin == null) {
+            $createdMin = '(UNIX_TIMESTAMP() - 3600)';
+        }
 
-        $join = 'JOIN `denkmal_model_message` m ON `m`.`id` = `denkmal_model_tag_model`.modelId';
+        $join = 'JOIN `denkmal_model_message` m ON `m`.`id` = `denkmal_model_tag_model`.`modelId` ';
+        $join .= 'AND `denkmal_model_tag_model`.`modelType` = ' . Denkmal_Model_Message::getTypeStatic();
 
         $group = '`denkmal_model_tag_model`.`tagId`';
 
-        $where = '`denkmal_model_tag_model`.`modelType` = ' . Denkmal_Model_Message::getTypeStatic();
-        $where .= ' AND `m`.`venue` = ' . $venue->getId();
-        $where .= ' AND `m`.`created` > (UNIX_TIMESTAMP() - 3600)';
+        $where = '`m`.`venue` = ' . $venue->getId();
+        $where .= ' AND `m`.`created` > ' . $createdMin;
 
         $source = new CM_PagingSource_Sql('`denkmal_model_tag_model`.tagId', 'denkmal_model_tag_model', $where, '`m`.`created` DESC', $join, $group);
         $source->enableCache();

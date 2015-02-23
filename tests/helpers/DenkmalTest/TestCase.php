6776a8157ce871a3092ad3daeeb1657f2f45650a
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 16:16:05 +0100

    Added processRequestWithViewer()

--- tests/helpers/DenkmalTest/TestCase.php
+++ tests/helpers/DenkmalTest/TestCase.php
@@ -3,6 +3,16 @@
 abstract class DenkmalTest_TestCase extends CMTest_TestCase {
 
     /**
+     * @param CM_Http_Request_Abstract|\Mocka\ClassTrait $request
+     * @param Denkmal_Model_User                         $user
+     * @return CM_Http_Response_Abstract|\Mocka\ClassTrait
+     */
+    public function processRequestWithViewer(CM_Http_Request_Abstract $request, Denkmal_Model_User $user) {
+        $request->mockMethod('getViewer')->set($user);
+        return $this->processRequest($request);
+    }
+
+    /**
      * @param int|null $role
      * @return Denkmal_Model_User
      */

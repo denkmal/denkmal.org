685f703068bfcf82016b29af45a4f81f23cd468c
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 22:21:18 +0100

    Added hipster tags paging

--- library/Denkmal/library/Denkmal/Paging/Tag/Venue/Hipster.php
+++ library/Denkmal/library/Denkmal/Paging/Tag/Venue/Hipster.php
@@ -6,11 +6,18 @@ class Denkmal_Paging_Tag_Venue_Hipster extends Denkmal_Paging_Tag_Abstract {
      * @param Denkmal_Model_Venue $venue
      */
     public function __construct(Denkmal_Model_Venue $venue) {
-        $where = '`denkmal_model_tag_model`.`modelType` = ' . Denkmal_Model_Message::getTypeStatic() . ' and `m`.`venue` = ' . $venue->getId();
+
         $join = 'JOIN `denkmal_model_message` m ON `m`.`id` = `denkmal_model_tag_model`.modelId';
 
-        $source = new CM_PagingSource_Sql('`denkmal_model_tag_model`.tagId', 'denkmal_model_tag_model', $where, '`m`.`created` DESC', $join);
+        $group = '`denkmal_model_tag_model`.`tagId`';
+
+        $where = '`denkmal_model_tag_model`.`modelType` = ' . Denkmal_Model_Message::getTypeStatic();
+        $where .= ' AND `m`.`venue` = ' . $venue->getId();
+        $where .= ' AND `m`.`created` > (UNIX_TIMESTAMP() - 3600)';
+
+        $source = new CM_PagingSource_Sql('`denkmal_model_tag_model`.tagId', 'denkmal_model_tag_model', $where, '`m`.`created` DESC', $join, $group);
         $source->enableCache();
+
         parent::__construct($source);
     }
 }
--- tests/library/Denkmal/Paging/Tag/Venue/HipsterTest.php
+++ tests/library/Denkmal/Paging/Tag/Venue/HipsterTest.php
@@ -6,27 +6,58 @@ class Denkmal_Paging_Tag_Venue_HipsterTest extends CMTest_TestCase {
         CMTest_TH::clearEnv();
     }
 
-    public function testGetItems() {
+    public function testGetTags() {
+        $venue = Denkmal_Model_Venue::create('Example', true, false);
+        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, new DateTime());
+
+        $tag1 = Denkmal_Model_Tag::create('foo');
+        $tag2 = Denkmal_Model_Tag::create('bar');
+
+        $message1->getTags()->add($tag1);
+        $message1->getTags()->add($tag2);
+
+        $tagList = new Denkmal_Paging_Tag_Venue_Hipster($venue);
+        $this->assertEquals(array($tag1, $tag2), $tagList);
+    }
+
+    public function testAggregation() {
         $venue = Denkmal_Model_Venue::create('Example', true, false);
 
-        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, new DateTime('2010-01-01'));
-        $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, new DateTime('2010-01-02'));
-        $message3 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 3', null, new DateTime('2010-01-03'));
+        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, new DateTime());
+        $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, new DateTime());
+        $message3 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 3', null, new DateTime());
 
         $tag1 = Denkmal_Model_Tag::create('foo');
         $tag2 = Denkmal_Model_Tag::create('bar');
         $tag3 = Denkmal_Model_Tag::create('foobar');
+        $tag4 = Denkmal_Model_Tag::create('barfoo');
 
         $message1->getTags()->add($tag1);
-        $message1->getTags()->add($tag2);
         $message1->getTags()->add($tag3);
 
         $message2->getTags()->add($tag2);
+        $message2->getTags()->add($tag3);
 
+        $message3->getTags()->add($tag4);
         $message3->getTags()->add($tag1);
-        $message3->getTags()->add($tag3);
 
         $tagList = new Denkmal_Paging_Tag_Venue_Hipster($venue);
-        $this->assertEquals(array($tag1, $tag3, $tag2, $tag1, $tag2, $tag3), $tagList);
+        $this->assertEquals(array($tag1, $tag3, $tag2, $tag4), $tagList);
+    }
+
+    public function testTimeLimit() {
+        $venue = Denkmal_Model_Venue::create('Example', true, false);
+
+        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, new DateTime('2014-02-22'));
+        $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, new DateTime());
+
+        $tag1 = Denkmal_Model_Tag::create('foo');
+        $tag2 = Denkmal_Model_Tag::create('bar');
+
+        $message1->getTags()->add($tag1);
+        $message2->getTags()->add($tag2);
+
+        $tagList = new Denkmal_Paging_Tag_Venue_Hipster($venue);
+        $this->assertEquals(array($tag2), $tagList);
     }
 }

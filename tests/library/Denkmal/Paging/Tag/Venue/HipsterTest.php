e24ad16486aa0616afc87dd32c71d5422164a5c1
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 22:59:08 +0100

    Updated tests;

--- tests/library/Denkmal/Paging/Tag/Venue/HipsterTest.php
+++ tests/library/Denkmal/Paging/Tag/Venue/HipsterTest.php
@@ -46,16 +46,23 @@ class Denkmal_Paging_Tag_Venue_HipsterTest extends CMTest_TestCase {
     public function testTimeLimit() {
         $venue = Denkmal_Model_Venue::create('Example', true, false);
 
-        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, (new DateTime('2014-02-22'))->sub(new DateInterval('PT2H')));
-        $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, new DateTime());
+        $message1 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 1', null, (new DateTime())->sub(new DateInterval('PT2H')));
+        $message2 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 2', null, (new DateTime())->sub(new DateInterval('PT30M')));
+        $message3 = Denkmal_Model_Message::create($venue, 'client', null, 'Message 3', null, new DateTime());
 
         $tag1 = Denkmal_Model_Tag::create('foo');
         $tag2 = Denkmal_Model_Tag::create('bar');
+        $tag3 = Denkmal_Model_Tag::create('zoo');
 
         $message1->getTags()->add($tag1);
         $message2->getTags()->add($tag2);
+        $message3->getTags()->add($tag3);
 
         $tagList = new Denkmal_Paging_Tag_Venue_Hipster($venue);
-        $this->assertEquals(array($tag2), $tagList);
+        $this->assertEquals(array($tag3, $tag2), $tagList);
+
+        $timeMin = (new DateTime())->sub(new DateInterval('PT10M'));
+        $tagList = new Denkmal_Paging_Tag_Venue_Hipster($venue, $timeMin->getTimestamp());
+        $this->assertEquals(array($tag3), $tagList);
     }
 }

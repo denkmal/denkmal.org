93292ce6959cdcd189670f612ddff10333a73b01
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2015-02-24 00:24:22 +0100

    Revert "hide songDetails after 2 seconds"
    This reverts commit 1489282410651ecb969dadf0ef349105e667be25.

--- library/Denkmal/library/Denkmal/Component/Event.js
+++ library/Denkmal/library/Denkmal/Component/Event.js
@@ -83,13 +83,6 @@ var Denkmal_Component_Event = Denkmal_Component_Abstract.extend({
    * @param {String} label
    */
   showSongDetails: function(label) {
-    var self = this;
-    var $songDetails = this.$('.songDetails');
-    $songDetails.find('.label').text(label);
-    this.$('.songDetails').transit({x: 0}, function() {
-      setTimeout(function() {
-        self.hideSongDetails();
-      }, 2000);
-    })
+    this.$('.songDetails').transit({x: 0}).find('.label').text(label);
   }
 });

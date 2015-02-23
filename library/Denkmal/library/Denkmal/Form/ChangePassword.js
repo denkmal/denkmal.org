7d3ab2540cecbf6961eb27763ed8208d15771a83
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 15:08:39 +0100

    Added ChangePassword form and action

---
+++ library/Denkmal/library/Denkmal/Form/ChangePassword.js
@@ -0,0 +1,7 @@
+/**
+ * @class Denkmal_Form_ChangePassword
+ * @extends CM_Form_Abstract
+ */
+var Denkmal_Form_ChangePassword = CM_Form_Abstract.extend({
+  _class: 'Denkmal_Form_ChangePassword'
+});

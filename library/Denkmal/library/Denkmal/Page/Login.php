6799dfc5fba4b9b2c6f3e5c4f691cd1e7127a235
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-23 12:39:07 +0100

    Removed checkAccessible from Denkmal_Page_Login

--- library/Denkmal/library/Denkmal/Page/Login.php
+++ library/Denkmal/library/Denkmal/Page/Login.php
@@ -1,13 +1,4 @@
 <?php
 
 class Denkmal_Page_Login extends Denkmal_Page_Abstract {
-
-    /** @var  Denkmal_Params */
-    protected $_params;
-
-    public function checkAccessible(CM_Frontend_Environment $environment) {
-        if (!$environment->getViewer(true)->getRoles()->contains(Denkmal_Role::ADMIN, Denkmal_Role::PUBLISHER)) {
-            throw new CM_Exception_AuthRequired();
-        }
-    }
 }

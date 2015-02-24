f2b76e57627c40b7733eac305ed3bf617e33a68e
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2015-02-24 11:44:28 +0100

    Replace EventDetails conent with new MessageList_Venue

--- library/Denkmal/library/Denkmal/Component/EventDetails.php
+++ library/Denkmal/library/Denkmal/Component/EventDetails.php
@@ -3,16 +3,9 @@
 class Denkmal_Component_EventDetails extends Denkmal_Component_Abstract {
 
     public function prepare(CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
-        $event = $this->_params->getEvent('event');
         $venue = $this->_params->getVenue('venue');
 
-        $now = Denkmal_Site::getCurrentDate();
-        $futureEvents = new Denkmal_Paging_Event_VenueDateInterval($venue, $now, null, array($event));
-        $futureEvents->setPage(1, 4);
-
-        $viewResponse->set('event', $event);
         $viewResponse->set('venue', $venue);
-        $viewResponse->set('futureEvents', $futureEvents);
 
         $this->_params = CM_Params::factory(); // Empty params to not send them to client
     }

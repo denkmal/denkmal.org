e3ddc6c7196e8697443953915369601b7893c672
Reto Kaiser <reto@cargomedia.ch>
2015-02-25 00:43:37 +0100

    Myspace not using "www"

--- resources/db/update/4.php
+++ resources/db/update/4.php
@@ -4,6 +4,6 @@ $rowList = CM_Db_Db::select('denkmal_model_link', '*', "`url` LIKE 'http://www.m
 foreach ($rowList as $row) {
     $id = $row['id'];
     $url = $row['url'];
-    $url = str_replace('http://www.myspace.com/', 'https://www.myspace.com/', $url);
+    $url = str_replace('http://www.myspace.com/', 'https://myspace.com/', $url);
     CM_Db_Db::update('denkmal_model_link', ['url' => $url], ['id' => $id]);
 }

5bd6f6b03cd12dbcd9cb64df74dcfe838bf51d60
Reto Kaiser <reto@cargomedia.ch>
2015-02-24 01:13:40 +0100

    Redirect to /chat on denkmal site

--- library/Denkmal/library/Denkmal/Site.php
+++ library/Denkmal/library/Denkmal/Site.php
@@ -39,6 +39,13 @@ class Denkmal_Site extends CM_Site_Abstract {
     }
 
     /**
+     * @return string
+     */
+    public function getLoginPage() {
+        return 'Denkmal_Page_Chat';
+    }
+
+    /**
      * @return int
      */
     public static function getDayOffset() {

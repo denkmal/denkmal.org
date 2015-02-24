d9eea4cb1a898355bb669800ddb6f1cff297d266
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2015-02-24 15:46:47 +0100

    Limit MessageList_Venue paging count

--- library/Denkmal/library/Denkmal/Component/MessageList/Abstract.php
+++ library/Denkmal/library/Denkmal/Component/MessageList/Abstract.php
@@ -4,7 +4,6 @@ abstract class Denkmal_Component_MessageList_Abstract extends Denkmal_Component_
 
     public function prepare(CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
         $messageList = $this->_params->getPaging('messageList');
-        $messageList->setPage(1, 100);
 
         $viewResponse->set('messageList', $messageList);
     }

c711532f081c35e104654313bbe0d544c5276dfe
Reto Kaiser <reto@cargomedia.ch>
2015-02-28 14:50:44 +0100

    Venue is mandatory for venue-messageList

--- library/Denkmal/library/Denkmal/Component/MessageList/Venue.php
+++ library/Denkmal/library/Denkmal/Component/MessageList/Venue.php
@@ -3,7 +3,7 @@
 class Denkmal_Component_MessageList_Venue extends Denkmal_Component_MessageList_Abstract {
 
     public function prepare(CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
-        $venue = $this->_params->has('venue') ? $this->_params->getVenue('venue') : null;
+        $venue = $this->_params->getVenue('venue');
         $messageList = new Denkmal_Paging_Message_Venue($venue);
         $messageList->setPage(1, $this->_params->getInt('count', 3));
 

ae958168c8abdf1b04c8826d8010d23ad4392e42
Vladimir Finko <fvovan@gmail.com>
2015-04-12 16:54:13 +0200

    Push subscription component model creation.

--- library/Denkmal/library/Denkmal/Component/PushNotifications.php
+++ library/Denkmal/library/Denkmal/Component/PushNotifications.php
@@ -3,12 +3,12 @@
 class Denkmal_Component_PushNotifications extends \Denkmal_Component_Abstract {
 
     public function prepare(\CM_Frontend_Environment $environment, \CM_Frontend_ViewResponse $viewResponse) {
-
     }
 
     public function ajax_storePush(CM_Params $params, CM_Frontend_JavascriptContainer_View $handler, CM_Http_Response_View_Ajax $response) {
         $subscriptionId = $params->getString('subscriptionId');
         $endpoint = $params->getString('endpoint');
         $user = $params->has('user') ? $params->getUser('user') : null;
+        Denkmal_Model_PushSubscription::create($subscriptionId, $endpoint, $user);
     }
 }

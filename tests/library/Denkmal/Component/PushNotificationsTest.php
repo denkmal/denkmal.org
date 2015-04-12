a5ba086bab23e66b504f1c30e211eb497f6cd175
Vladimir Finko <fvovan@gmail.com>
2015-04-12 15:54:02 +0200

    Draft for push notifications backend

---
+++ tests/library/Denkmal/Component/PushNotificationsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class Denkmal_Component_PushNotificationsTest extends CMTest_TestCase {
+
+    public function testAjax_storePush() {
+        $environment = new CM_Frontend_Environment();
+        $response = $this->getResponseAjax(new Denkmal_Component_PushNotifications(),
+            'storePush',
+            [
+                'subscriptionId' => '123321',
+                'endpoint'       => 'https://google.com/push',
+                'user'           => null,
+            ],
+            $environment
+        );
+
+        $this->assertViewResponseSuccess($response);
+    }
+}

ba7a298665c8fa1e78d703defb06b4120a0e0624
Reto Kaiser <reto@cargomedia.ch>
2015-04-25 19:42:49 +0200

    Formatting

--- resources/config/internal.php
+++ resources/config/internal.php
@@ -136,7 +136,7 @@ return function (CM_Config_Node $config) {
     $config->Denkmal_Push_Subscription->type = 63;
     $config->Denkmal_Push_Notification_Message->type = 64;
     $config->CM_Class_Abstract->typesMaxValue = 64;
-
+    
     $config->CM_Action_Abstract->verbs = [];
     $config->CM_Action_Abstract->verbs[CM_Action_Abstract::CREATE] = 1;
     $config->CM_Action_Abstract->verbs[CM_Action_Abstract::UPDATE] = 2;
@@ -152,5 +152,5 @@ return function (CM_Config_Node $config) {
     $config->CM_Action_Abstract->verbs[CM_Action_Abstract::UNSUBSCRIBE] = 12;
     $config->CM_Action_Abstract->verbs[CM_Action_Abstract::SEND] = 13;
     $config->CM_Action_Abstract->verbsMaxValue = 13;
-
+    
 };

0e7478feb6427d8bc038a53ec010d56548eb26cc
Reto Kaiser <reto@cargomedia.ch>
2015-12-21 21:45:49 +0100

    Update sääli scraper

--- tests/library/Denkmal/Scraper/Source/SaaliTest.php
+++ tests/library/Denkmal/Scraper/Source/SaaliTest.php
@@ -37,4 +37,31 @@ class Denkmal_Scraper_Source_SaaliTest extends CMTest_TestCase {
 
         $this->assertCount(0, $eventDataList);
     }
+
+    public function testProcessVersion2() {
+        $html = Denkmal_Scraper_Source_Abstract::loadFile(DIR_TEST_DATA . 'scraper/saali-2.html');
+        $scraper = new Denkmal_Scraper_Source_Saali();
+
+        $eventDataList = $scraper->processPage($html, 2015);
+
+        $this->assertCount(3, $eventDataList);
+
+        $this->assertEquals(new Denkmal_Scraper_EventData(
+            'Sääli',
+            new Denkmal_Scraper_Description('Quiz der Populärkultur Mit Mämä Sykora & Sascha Török'),
+            new DateTime('2015-12-12 21:00:00')
+        ), $eventDataList[0]);
+
+        $this->assertEquals(new Denkmal_Scraper_EventData(
+            'Sääli',
+            new Denkmal_Scraper_Description('Extrafish (LU) Winterfishtour Gipsy_Swing'),
+            new DateTime('2015-12-17 21:00:00')
+        ), $eventDataList[1]);
+
+        $this->assertEquals(new Denkmal_Scraper_EventData(
+            'Sääli',
+            new Denkmal_Scraper_Description('Starmachine (CH) Live Karaoke-Band'),
+            new DateTime('2015-12-19 21:00:00')
+        ), $eventDataList[2]);
+    }
 }

ab62dd7b4dc789630c0044956c37b107ae8d6942
Reto Kaiser <reto@cargomedia.ch>
2016-05-20 18:12:56 +0200

    Upgrade to new logger

--- library/Admin/library/Admin/Page/Log.php
+++ library/Admin/library/Admin/Page/Log.php
@@ -3,11 +3,11 @@
 class Admin_Page_Log extends Admin_Page_Abstract {
 
     public function prepare(CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
-        $type = $this->_params->getInt('type');
+        $level = $this->_params->has('level') ? $this->_params->getInt('level') : null;
         $aggregate = $this->_params->has('aggregate') ? $this->_params->getInt('aggregate') : null;
         $page = $this->_params->getPage();
 
-        $viewResponse->set('type', $type);
+        $viewResponse->set('level', $level);
         $viewResponse->set('aggregate', $aggregate);
         $viewResponse->set('page', $page);
     }

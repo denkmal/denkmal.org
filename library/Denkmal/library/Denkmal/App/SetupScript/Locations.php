149433e8e575ba1ad799c4c6c3a453d03d386945
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-05-21 13:39:08 +0200

    add basel region to setup

--- library/Denkmal/library/Denkmal/App/SetupScript/Locations.php
+++ library/Denkmal/library/Denkmal/App/SetupScript/Locations.php
@@ -5,10 +5,11 @@ class Denkmal_App_SetupScript_Locations extends CM_Provision_Script_OptionBased 
     public function load(CM_OutputStream_Interface $output) {
         $country = CM_Model_Location::createCountry('Switzerland', 'CH');
         $state = CM_Model_Location::createState($country, 'Basel-Stadt', null, 'CH04');
-        CM_Model_Location::createCity($state, 'Basel', 47.5584, 7.5733, '16518');
+        $basel = CM_Model_Location::createCity($state, 'Basel', 47.5584, 7.5733, '16518');
         $country = CM_Model_Location::createCountry('Austria', 'AT');
         $state = CM_Model_Location::createState($country, 'Steiermark', null, 'AT06');
         CM_Model_Location::createCity($state, 'Graz', 47.0667, 15.45, '16516');
+        Denkmal_Model_Region::create('Basel', 'basel', 'BSL', $basel);
 
         $this->_setLoaded(true);
     }

cc80bff14041f38f7dc22d8a528cb6f3615dc945
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-05-21 14:18:44 +0200

    refactor setup

--- library/Denkmal/library/Denkmal/App/SetupScript/Locations.php
+++ library/Denkmal/library/Denkmal/App/SetupScript/Locations.php
@@ -3,13 +3,16 @@
 class Denkmal_App_SetupScript_Locations extends CM_Provision_Script_OptionBased {
 
     public function load(CM_OutputStream_Interface $output) {
-        $country = CM_Model_Location::createCountry('Switzerland', 'CH');
-        $state = CM_Model_Location::createState($country, 'Basel-Stadt', null, 'CH04');
-        $basel = CM_Model_Location::createCity($state, 'Basel', 47.5584, 7.5733, '16518');
-        $country = CM_Model_Location::createCountry('Austria', 'AT');
-        $state = CM_Model_Location::createState($country, 'Steiermark', null, 'AT06');
-        CM_Model_Location::createCity($state, 'Graz', 47.0667, 15.45, '16516');
+        $switzerland = CM_Model_Location::createCountry('Switzerland', 'CH');
+        $baselStadt = CM_Model_Location::createState($switzerland, 'Basel-Stadt', null, 'CH04');
+        $basel = CM_Model_Location::createCity($baselStadt, 'Basel', 47.5584, 7.5733, '16518');
+
+        $austria = CM_Model_Location::createCountry('Austria', 'AT');
+        $steiermark = CM_Model_Location::createState($austria, 'Steiermark', null, 'AT06');
+        $graz = CM_Model_Location::createCity($steiermark, 'Graz', 47.0667, 15.45, '16516');
+        
         Denkmal_Model_Region::create('Basel', 'basel', 'BSL', $basel);
+        Denkmal_Model_Region::create('Graz', 'graz', 'GRZ', $graz);
 
         $this->_setLoaded(true);
     }

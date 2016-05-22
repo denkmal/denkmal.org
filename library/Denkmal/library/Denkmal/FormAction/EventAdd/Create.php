99a6c21895081f1db0021a31a87247347382e492
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-05-22 17:23:20 +0200

    save correct venue's region when adding an event

--- library/Denkmal/library/Denkmal/FormAction/EventAdd/Create.php
+++ library/Denkmal/library/Denkmal/FormAction/EventAdd/Create.php
@@ -24,10 +24,11 @@ class Denkmal_FormAction_EventAdd_Create extends CM_FormAction_Abstract {
     }
 
     protected function _process(CM_Params $params, CM_Http_Response_View_Form $response, CM_Form_Abstract $form) {
-        /** @var Denkmal_Params $params */
-        $params = $form->getParams();
-        $region = $params->getRegion('region');
+        /** @var Denkmal_Params $formParams */
+        $formParams = $form->getParams();
+        $region = $formParams->getRegion('region');
 
+        /** @var Denkmal_Params $params */
         $venue = Denkmal_Form_EventAdd::getVenueFromData($params, $region);
         $venue->commit();
 

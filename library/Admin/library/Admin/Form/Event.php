f12886d82669a4d868617667edb08da3cbc1015e
Reto Kaiser <reto@cargomedia.ch>
2016-05-31 04:28:42 +0900

    Pass timeZone to form fields

--- library/Admin/library/Admin/Form/Event.php
+++ library/Admin/library/Admin/Form/Event.php
@@ -3,11 +3,16 @@
 class Admin_Form_Event extends CM_Form_Abstract {
 
     protected function _initialize() {
+        /** @var Denkmal_Params $params */
+        $params = $this->getParams();
+        $event = $params->getEvent('event');
+        $timeZone = $event->getTimeZone();
+
         $this->registerField(new CM_FormField_Hidden(['name' => 'eventId']));
         $this->registerField(new Denkmal_FormField_Venue(['name' => 'venue', 'enableChoiceCreate' => false]));
-        $this->registerField(new CM_FormField_Date(['name' => 'date', 'yearFirst' => date('Y') - 1, 'yearLast' => (int) date('Y') + 1]));
-        $this->registerField(new Denkmal_FormField_Time(['name' => 'fromTime']));
-        $this->registerField(new Denkmal_FormField_Time(['name' => 'untilTime']));
+        $this->registerField(new CM_FormField_Date(['name' => 'date', 'timeZone' => $timeZone, 'yearFirst' => date('Y') - 1, 'yearLast' => (int) date('Y') + 1]));
+        $this->registerField(new CM_FormField_Time(['name' => 'fromTime', 'timeZone' => $timeZone]));
+        $this->registerField(new CM_FormField_Time(['name' => 'untilTime', 'timeZone' => $timeZone]));
         $this->registerField(new CM_FormField_Textarea(['name' => 'description']));
         $this->registerField(new Denkmal_FormField_Song(['name' => 'song']));
         $this->registerField(new CM_FormField_Boolean(['name' => 'starred']));

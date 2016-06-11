5cb9c532a8475108258dcf64614a99cdd23345d7
Reto Kaiser <reto@cargomedia.ch>
2016-06-11 15:12:18 +0200

    Send message notifications only to subscribers of region's site

--- library/Denkmal/library/Denkmal/MessagePushNotification/SendJob.php
+++ library/Denkmal/library/Denkmal/MessagePushNotification/SendJob.php
@@ -5,18 +5,22 @@ class Denkmal_MessagePushNotification_SendJob extends CM_Jobdistribution_Job_Abs
     protected function _execute(CM_Params $params) {
         /** @var Denkmal_Params $params */
         $message = $params->getMessage('message');
+        $region = $message->getVenue()->getRegion();
+        $site = Denkmal_Site_Region_Abstract::getSiteByRegion($region);
 
         $serviceManager = CM_Service_Manager::getInstance();
         /** @var Denkmal_Push_Notification_Sender $sender */
         $sender = $serviceManager->get('push-notification-sender', 'Denkmal_Push_Notification_Sender');
-        $render = new CM_Frontend_Render(null, null, $serviceManager);
+
+        $environment = new CM_Frontend_Environment($site);
+        $render = new CM_Frontend_Render($environment, null, $serviceManager);
         $formatter = new Denkmal_MessagePushNotification_Formatter($render);
 
         $pushNotificationMessage = new Denkmal_Push_Notification_Message();
         $pushNotificationMessage->setExpires((new DateTime())->add(new DateInterval('PT6H')));
         $pushNotificationMessage->setData($formatter->getPushData($message));
 
-        $subscriptionList = new Denkmal_Push_SubscriptionList_All();
+        $subscriptionList = new Denkmal_Push_SubscriptionList_Site($site);
         $sender->sendNotifications($subscriptionList->getItems(), $pushNotificationMessage);
     }
 }

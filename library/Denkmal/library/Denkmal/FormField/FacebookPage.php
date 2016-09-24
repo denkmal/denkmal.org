c6c24b67ff36ce3a96758d383bc751cdac00db43
Reto Kaiser <reto@cargomedia.ch>
2016-09-24 15:26:05 +0200

    Retrieve page ID from URL in venue field

--- library/Denkmal/library/Denkmal/FormField/FacebookPage.php
+++ library/Denkmal/library/Denkmal/FormField/FacebookPage.php
@@ -2,13 +2,39 @@
 
 class Denkmal_FormField_FacebookPage extends CM_FormField_Text {
 
+    protected function _initialize() {
+        /** @var Denkmal_Params $params */
+        $params = $this->getParams();
+        $this->_options['region'] = $params->getRegion('region');
+        parent::_initialize();
+    }
+
     public function validate(\CM_Frontend_Environment $environment, $userInput) {
         $userInput = parent::validate($environment, $userInput);
+
+        $region = $this->_getRegion();
+        $facebookAccessToken = $region->getFacebookAccessToken();
+
+        if (preg_match('#^http#', $userInput) && $facebookAccessToken) {
+            $client = new \Facebook\FacebookClient();
+            $request = new Denkmal_Facebook_RequestWithoutApp(null, $facebookAccessToken, 'GET', '/' . $userInput);
+            $response = $client->sendRequest($request);
+            if ($pageId = $response->getGraphPage()->getId()) {
+                $userInput = $pageId;
+            }
+        }
 
         if (preg_match('#[^\d]#', $userInput)) {
             throw new CM_Exception_FormFieldValidation(new CM_I18n_Phrase('Should only contain digits.'));
         }
 
         return $userInput;
+    }
+
+    /**
+     * @return Denkmal_Model_Region
+     */
+    private function _getRegion() {
+        return $this->_options['region'];
     }
 }

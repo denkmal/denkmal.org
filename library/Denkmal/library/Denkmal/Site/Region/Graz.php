ac11b46b62e40e842af7847bd3a621e5bd8e7d49
Reto Kaiser <reto@cargomedia.ch>
2016-09-29 19:07:02 +0200

    Enable Graz site

--- library/Denkmal/library/Denkmal/Site/Region/Graz.php
+++ library/Denkmal/library/Denkmal/Site/Region/Graz.php
@@ -2,10 +2,6 @@
 
 class Denkmal_Site_Region_Graz extends Denkmal_Site_Region_Abstract {
 
-    public function isEnabled() {
-        return CM_Bootloader::getInstance()->isDebug();
-    }
-
     protected function _getRegionSlug() {
         return 'graz';
     }

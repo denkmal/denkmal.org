2929b914800683ceadac729e77126303f0e0844c
Reto Kaiser <reto@retokaiser.com>
2017-01-01 16:54:08 +0100

    Pass venue name in google maps link

--- tests/library/Denkmal/Component/EventDetailsTest.php
+++ tests/library/Denkmal/Component/EventDetailsTest.php
@@ -7,13 +7,13 @@ class Denkmal_Component_EventDetailsTest extends CMTest_TestCase {
     }
 
     public function testRender() {
-        $venue = DenkmalTest_TH::createVenue(null, null, null, null, null, null, new CM_Geo_Point(12, 13));
+        $venue = DenkmalTest_TH::createVenue('My Venue', null, null, null, null, null, new CM_Geo_Point(12, 13));
         $component = new Denkmal_Component_EventDetails(['venue' => $venue]);
         $html = $this->_renderComponent($component);
 
         $this->assertComponentAccessible($component);
         $this->assertTrue($html->has('a.location'));
-        $this->assertSame('https://www.google.com/maps/?q=12,13', $html->find('a.location')->getAttribute('href'));
+        $this->assertSame('https://www.google.com/maps?ll=12%2C13&q=My+Venue', $html->find('a.location')->getAttribute('href'));
     }
 
     public function testSecretVenue() {

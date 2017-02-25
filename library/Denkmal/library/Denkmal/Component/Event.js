990df1d84d8a9ad01d334bb0047cf29573841380
Reto Kaiser <reto@retokaiser.com>
2017-02-25 17:23:01 +0100

    Use separate class for bookmark animation

--- library/Denkmal/library/Denkmal/Component/Event.js
+++ library/Denkmal/library/Denkmal/Component/Event.js
@@ -43,6 +43,9 @@ var Denkmal_Component_Event = Denkmal_Component_Abstract.extend({
   _updateVenueBookmark: function(state) {
     var $el = this.$('.venue-bookmark');
     state ? $el.attr('data-bookmarked', '') : $el.removeAttr('data-bookmarked');
+    if (state) {
+      $el.removeClass('bookmark-animation').triggerReflow().addClass('bookmark-animation');
+    }
   }
 
 });

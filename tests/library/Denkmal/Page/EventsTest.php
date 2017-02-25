c9f6338eb494f01d08652e75ed4e6b284bd5917d
Reto Kaiser <reto@retokaiser.com>
2017-02-25 16:17:19 +0100

    Adjust events page test

--- tests/library/Denkmal/Page/EventsTest.php
+++ tests/library/Denkmal/Page/EventsTest.php
@@ -12,12 +12,19 @@ class Denkmal_Page_EventsTest extends CMTest_TestCase {
     }
 
     public function testVenueBookmarks() {
+        $date = new DateTime('2017-01-01 20:00');
         $region = Denkmal_Model_Region::findBySlug('basel');
-        $venue1 = DenkmalTest_TH::createVenue('Venue 1', null, null, $region);
-        $venue2 = DenkmalTest_TH::createVenue('Venue 2', null, null, $region);
-        $venue3 = DenkmalTest_TH::createVenue('Venue 3', null, null, $region);
 
-        $page = new Denkmal_Page_Events();
+        $venue1 = DenkmalTest_TH::createVenue('Venue 1', null, null, $region);
+        $event1 = Denkmal_Model_Event::create($venue1, 'Event 1', true, true, $date);
+
+        $venue2 = DenkmalTest_TH::createVenue('Venue 2', null, null, $region);
+        $event2 = Denkmal_Model_Event::create($venue2, 'Event 2', true, true, $date);
+
+        $venue3 = DenkmalTest_TH::createVenue('Venue 3', null, null, $region);
+        $event3 = Denkmal_Model_Event::create($venue3, 'Event 3', true, true, $date);
+
+        $page = new Denkmal_Page_Events(['date' => $date]);
         $site = new Denkmal_Site_Region_Basel();
         $host = parse_url($site->getUrl(), PHP_URL_HOST);
         $request = new CM_Http_Request_Get('', [
@@ -30,7 +37,9 @@ class Denkmal_Page_EventsTest extends CMTest_TestCase {
 
         $html = new CM_Dom_NodeList($renderAdapter->fetch(), true);
 
-        $this->assertTrue($html->has('[venue-id="' . $venue1->getId() . '"] .venue-bookmark[bookmarked]'));
+        $this->assertSame('', $html->find('.venue-bookmark[data-venue-id="' . $venue1->getId() . '"]')->getAttribute('data-bookmarked'));
+        $this->assertSame(null, $html->find('.venue-bookmark[data-venue-id="' . $venue2->getId() . '"]')->getAttribute('data-bookmarked'));
+        $this->assertSame('', $html->find('.venue-bookmark[data-venue-id="' . $venue3->getId() . '"]')->getAttribute('data-bookmarked'));
     }
 
 }

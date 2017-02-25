827ef2043c830557f0df1990fe1d8da9679a0948
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2017-02-25 15:14:15 +0100

    Fix tests

--- tests/library/Denkmal/EventFormatter/EventFormatterTest.php
+++ tests/library/Denkmal/EventFormatter/EventFormatterTest.php
@@ -12,8 +12,8 @@ class Denkmal_EventFormatter_EventFormatterTest extends CMTest_TestCase {
 
         Denkmal_Model_EventCategory::create('pop', new CM_Color_RGB(0, 255, 0), ['pop', 'funk']);
 
-        $this->assertSame(
-            'My &lt;&gt; description. Rock &lt;&gt; <span class="genre" style="background-color:#00FF00;">pop</span>',
+        $this->assertRegExp(
+            '|My &lt;&gt; description. Rock &lt;&gt; <span class="genre" style="([^"]*)#00FF00([^"]*)">pop</span>|',
             $eventFormatter->getHtml($event)
         );
 
--- tests/library/Denkmal/EventFormatter/GenresFilterTest.php
+++ tests/library/Denkmal/EventFormatter/GenresFilterTest.php
@@ -11,13 +11,13 @@ class Denkmal_EventFormatter_GenresFilterTest extends CMTest_TestCase {
             $filter->transform('foo1, foo2, bar', $render));
 
         $eventCategory = Denkmal_Model_EventCategory::create('cat-foo', new CM_Color_RGB(255, 0, 0), ['foo1']);
-        $this->assertSame(
-            '<span class="genre" style="background-color:#FF0000;">foo1</span>, foo2, bar',
+        $this->assertRegExp(
+            '|<span class="genre" style="([^"]*)#FF0000([^"]*)">foo1</span>, foo2, bar|',
             $filter->transform('foo1, foo2, bar', $render));
 
         $eventCategory->addGenre('foo2');
-        $this->assertSame(
-            '<span class="genre" style="background-color:#FF0000;">foo1</span>, <span class="genre" style="background-color:#FF0000;">FOO2</span>, bar',
+        $this->assertRegExp(
+            '|<span class="genre" style="([^"]*)#FF0000([^"]*)">foo1</span>, <span class="genre" style="([^"]*)#FF0000([^"]*)">FOO2</span>, bar|',
             $filter->transform('foo1, FOO2, bar', $render));
     }
 }

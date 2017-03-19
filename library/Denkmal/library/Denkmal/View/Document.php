523f82ff171546808e39f0d4484ed15329f2a8eb
Reto Kaiser <reto@retokaiser.com>
2017-03-19 13:51:52 +0100

    Add Document.getCurrentDate() client side implementation

--- library/Denkmal/library/Denkmal/View/Document.php
+++ library/Denkmal/library/Denkmal/View/Document.php
@@ -2,4 +2,19 @@
 
 class Denkmal_View_Document extends CM_View_Document {
 
+    public function prepare(CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
+        parent::prepare($environment, $viewResponse);
+
+        /** @var Denkmal_Site_Default $site */
+        $site = $environment->getSite();
+        $appSettings = new Denkmal_App_Settings();
+        $timeZone = null;
+        if ($site->hasRegion()) {
+            $timeZone = $site->getRegion()->getTimeZone()->getName();
+        }
+
+        $viewResponse->getJs()->setProperty('dayOffset', $appSettings->getDayOffset());
+        $viewResponse->getJs()->setProperty('timeZone', $timeZone);
+    }
+
 }

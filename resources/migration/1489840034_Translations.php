141b4fc2321998df40ede358bde68e649e0160cf
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2017-03-19 10:17:28 +0100

    Re-run translation setup script to update translations

--- resources/migration/1489840034_Translations.php
+++ resources/migration/1489840034_Translations.php
@@ -5,12 +5,7 @@ class Migration_1489840034_Translations implements \CM_Migration_UpgradableInter
     use CM_Service_ManagerAwareTrait;
 
     public function up(\CM_OutputStream_Interface $output) {
-        CM_Model_LanguageKey::deleteByName('.meta.description');
-        CM_Model_LanguageKey::deleteByName('.meta.description.basel');
-        CM_Model_LanguageKey::deleteByName('.meta.description.graz');
-        
-        CM_Model_Language::findDefault()->setTranslation('.meta.description', '{$siteName} is an event calendar made and curated by locals. We help you explore your city\'s nightlife and make sure you never miss a great party again!');
-        CM_Model_Language::findDefault()->setTranslation('.meta.description.basel', 'Basel\'s event calendar made and curated by locals. We help you explore Basel\'s nightlife and make sure you never miss a great party again!');
-        CM_Model_Language::findDefault()->setTranslation('.meta.description.graz', 'Graz\' event calendar made and curated by locals. We help you explore Graz\' nightlife and make sure you never miss a great party again!');
+        $translationSetup = new CM_App_SetupScript_Translations($this->getServiceManager());
+        $translationSetup->load($output);
     }
 }

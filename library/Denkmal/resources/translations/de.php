14f3c88346bb283f376968d1bb685e215f1e0f0b
Reto Kaiser <reto@retokaiser.com>
2017-03-22 19:35:30 +0100

    Update copy of meta texts

--- library/Denkmal/resources/translations/de.php
+++ library/Denkmal/resources/translations/de.php
@@ -4,7 +4,7 @@ return function (CM_Model_Language $language) {
     $language->setTranslation('.internals.role.' . Denkmal_Role::ADMIN, 'Admin');
     $language->setTranslation('.internals.role.' . Denkmal_Role::PUBLISHER, 'Publisher');
 
-    $language->setTranslation('.meta.description', '{$siteName} wird von Locals gemacht. Entdecke das Nachtleben deiner Stadt und verpasse keine erstklassige Party mehr!');
+    $language->setTranslation('.meta.description', '{$siteName} wird von Locals gemacht. Entdecke das Nachtleben deiner Stadt und verpasse keine spannenden Events.');
     $language->setTranslation('.meta.description.basel', 'Basels Eventkalender. WaslOift am Rheinknie und wie hört es sich an?');
     $language->setTranslation('.meta.description.graz', 'Events in Graz. Was geht in der Metropole an der Mur?');
 };
--- library/Denkmal/resources/translations/en.php
+++ library/Denkmal/resources/translations/en.php
@@ -4,7 +4,7 @@ return function (CM_Model_Language $language) {
     $language->setTranslation('.internals.role.' . Denkmal_Role::ADMIN, 'Admin');
     $language->setTranslation('.internals.role.' . Denkmal_Role::PUBLISHER, 'Publisher');
 
-    $language->setTranslation('.meta.description', '{$siteName} is an event calendar by locals. Explore your city\'s nightlife and get event updates and impressions by the crowd in real-time.');
-    $language->setTranslation('.meta.description.basel', 'Basel\'s event calendar made and curated by locals. We help you explore Basel\'s nightlife and make sure you never miss a great party again!');
-    $language->setTranslation('.meta.description.graz', 'Graz\' event calendar made and curated by locals. We help you explore Graz\' nightlife and make sure you never miss a great party again!');
+    $language->setTranslation('.meta.description', '{$siteName} is an event calendar made by locals. Explore your city\'s nightlife and never miss a great party again.');
+    $language->setTranslation('.meta.description.basel', 'Basel\'s event calendar made by locals. Explore Basel\'s nightlife and never miss a great party again.');
+    $language->setTranslation('.meta.description.graz', 'Graz\' event calendar made by locals. Explore Graz\' nightlife and never miss a great party again.');
 };
